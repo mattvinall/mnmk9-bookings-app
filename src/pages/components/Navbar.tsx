c99f1bb5f850d64e07ca6f7c41c20efcff3d408b
Matt Vinall <matt.vinall7@gmail.com>
2022-12-14 10:38:31 -0500

    leveraged variables to access name and image from google session

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from 'next/image'
 import logo from "../../../public/mnmk9-logo.jpg";
@@ -15,12 +15,14 @@ const MyImage = () => {
 
 const AuthShowcase: React.FC = () => {
 	const { data: sessionData } = useSession();
-
+	const userImage = sessionData && sessionData?.user?.image;
+	const userName = sessionData && sessionData?.user?.name
 	return (
 		<div className="flex items-center">
-			<p className="text-center text-small text-purple px-12">
-				{sessionData && <span>{sessionData.user?.name}</span>}
+			<p className="text-small text-purple pl-24">
+				{sessionData && <span className="font-semibold">{userName}</span>}
 			</p>
+			{userImage && <span><img className="rounded-full scale-50 float-right" src={userImage} /></span>}
 			<button
 				className="rounded-full bg-gradient-to-b from-[#2e026d] to-[#15162c] px-10 py-3 font-semibold text-white no-underline transition hover:bg-gradient-to-b from-[#15162c] to-[#2e026d]"
 				onClick={sessionData ? () => signOut() : () => signIn()}
@@ -31,12 +33,18 @@ const AuthShowcase: React.FC = () => {
 	);
 };
 
+
 const Navbar = () => {
+	const [menuToggled, setMenuToggled] = useState(false);
+	const handleClick = () => {
+		console.log("icon clicked", menuToggled);
+		setMenuToggled((prevState) => !prevState);
+	}
 	return (
 		<nav className="bg-white shadow-lg">
 			<div className="max-w-6xl mx-auto px-4">
 				<div className="flex justify-between align-start">
-					<div className="flex items-center space-x-24 h-32">
+					<div className="flex items-center h-32">
 						<div>
 							<a href="https://www.mnmk9.ca" className="flex items-center py-4 px-2">
 								<MyImage />
@@ -44,17 +52,40 @@ const Navbar = () => {
 						</div>
 						<div className="hidden md:flex items-center space-x-1">
 							<a
-								href=""
-								className="py-4 px-4 text-purple-500 border-b-4 border-green-500 font-semibold ">Book a Service</a>
+								href="/create-booking"
+								className="py-4 px-4 text-purple-700 border-b-4 border-green-500 font-semibold ">Book a Service</a>
 							<a
-								href=""
-								className="py-4 px-4 text-purple-500 font-semibold hover:text-green-500 transition duration-300">Manage A Service</a>
+								href="/manage-booking"
+								className="py-4 px-4 text-purple-700 font-semibold hover:text-green-500 transition duration-300">Manage A Service</a>
 							<a
-								href=""
-								className="py-4 px-4 text-purple-500 font-semibold hover:text-green-500 transition duration-300">Contact Us</a>
+								href="/contact"
+								className="py-4 px-4 text-purple-700 font-semibold hover:text-green-500 transition duration-300">Contact Us</a>
 						</div>
 						<AuthShowcase />
+						<div className="md:hidden flex items-center">
+							<button className="outline-none mobile-menu-button" onClick={handleClick}>
+								<svg className=" w-6 h-6 text-gray-500 hover:text-green-500 "
+									x-show="!showMenu"
+									fill="none"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+								>
+									<path d="M4 6h16M4 12h16M4 18h16"></path>
+								</svg>
+							</button>
+						</div>
 					</div>
+				</div>
+
+				<div className={`${!menuToggled ? "hidden" : ""} mobile-menu`}>
+					<ul className="">
+						<li className="active"><a href="/create-booking" className="block text-md px-2 py-4 text-white bg-green-500 font-semibold">Book a Service</a></li>
+						<li><a href="/manage-booking" className="block text-md px-2 py-4 hover:bg-green-500 transition duration-300">Manage A Service</a></li>
+						<li><a href="/contact" className="block text-md px-2 py-4 hover:bg-green-500 transition duration-300">Contact Us</a></li>
+					</ul>
 				</div>
 			</div>
 		</nav>
