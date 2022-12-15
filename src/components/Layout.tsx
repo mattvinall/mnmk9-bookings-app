9d5cfd9a1d8a0857dee8ebbd7b49e1cfd00617bc
Matt Vinall <matt.vinall7@gmail.com>
2022-12-15 15:26:15 -0500

    created a MainLayout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode } from 'react'
+import Head from "next/head";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+
+interface ILayoutProps {
+	children: NonNullable<ReactNode>;
+}
+
+const MainLayout = ({ children }: ILayoutProps) => {
+	return (
+		<>
+			<Head>
+				<title>MNMK-9 Bookings App</title>
+				<meta name="description" content="MNMK-9 Booking App" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Navbar />
+			<main className="lex min-h-[85vh] flex justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+				{children}
+			</main>
+			<Footer />
+		</>
+	)
+}
+
+export default MainLayout;
