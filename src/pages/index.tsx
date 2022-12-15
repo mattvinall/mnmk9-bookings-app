9647bd8d1fdc07c8fc72fc4615b2674aa24c916e
Matt Vinall <matt.vinall7@gmail.com>
2022-12-15 15:27:30 -0500

    removed all markup and added it to its own ShowCase component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,11 @@
 import { type NextPage } from "next";
-import Navbar from "../components/Navbar";
-import Head from "next/head";
-import Link from "next/link";
-
-import { trpc } from "../utils/trpc";
+import HomeShowCase from "../components/HomeShowCase"
 
 const Home: NextPage = () => {
-  const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
 
   return (
     <>
-      <Head>
-        <title>MNMK-9 Bookings App</title>
-        <meta name="description" content="MNMK-9 Booking App" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Navbar />
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            MNMK-9 <span className="text-[hsl(280,100%,70%)]">Bookings</span>
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="/create-booking"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Create Booking →</h3>
-              <div className="text-lg">
-                Book a service
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="/manage-booking"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Manage a Booking →</h3>
-              <div className="text-lg">
-                Manage a booking
-              </div>
-            </Link>
-          </div>
-          <div className="flex flex-col items-center gap-2">
-          </div>
-        </div>
-      </main>
+      <HomeShowCase />
     </>
   );
 };
