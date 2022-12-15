96b5589b0c1ebe53b3f0c85addf6394dbef7b775
Matt Vinall <matt.vinall7@gmail.com>
2022-12-15 16:33:01 -0500

    added title for manage a booking with heading styles

--- src/pages/manage-booking.tsx
+++ src/pages/manage-booking.tsx
@@ -4,7 +4,9 @@ type Props = {}
 
 const ManageBooking = (props: Props) => {
 	return (
-		<h1 className="heading">Manage Booking</h1>
+		<h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] py-16">
+			<span className="text-[hsl(280,100%,70%)]">Manage a Booking</span>
+		</h1>
 	)
 }
 
