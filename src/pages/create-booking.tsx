3b93caa25ec9078fcf2048579cdbeb3e89842ae3
Matt Vinall <matt.vinall7@gmail.com>
2022-12-15 10:41:05 -0500

    imported Navbar to this page

--- src/pages/create-booking.tsx
+++ src/pages/create-booking.tsx
@@ -1,7 +1,7 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
+import Navbar from "./components/Navbar";
 
 import { trpc } from "../utils/trpc";
 
@@ -15,37 +15,34 @@ const CreateBooking: NextPage = () => {
         <meta name="description" content="MNMK-9 Booking App" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar />
       <main className="flex min-h-screen flex justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-start gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          	<span className="text-[hsl(280,100%,70%)]">Book a Service</span> 
+            <span className="text-[hsl(280,100%,70%)]">Book a Service</span>
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-4 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="/create-booking"
-              target="_blank"
+              href="/create-booking/boarding"
             >
               <h3 className="text-2xl font-bold text-center">Boarding</h3>
             </Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="/manage-booking"
-              target="_blank"
+              href="/create-booking/daycare"
             >
               <h3 className="text-2xl font-bold text-center">Daycare</h3>
-						</Link>
-						<Link
+            </Link>
+            <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
+              href="/create-booking/grooming"
             >
               <h3 className="text-2xl font-bold text-center">Grooming</h3>
-						</Link>
-						<Link
+            </Link>
+            <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
+              href="/create-booking/training"
             >
               <h3 className="text-2xl font-bold text-center">Training</h3>
             </Link>
