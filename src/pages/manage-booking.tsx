0185a4de05fe40182a3f075be3f28a28ef0e561d
Matt Vinall <matt.vinall7@gmail.com>
2022-12-15 10:41:45 -0500

    placeholder content for manage booking page

---
+++ src/pages/manage-booking.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const ManageBooking = (props: Props) => {
+	return (
+		<h1 className="heading">Manage Booking</h1>
+	)
+}
+
+export default ManageBooking
