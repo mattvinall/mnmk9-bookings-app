a6b18170cf56df1781fca6fb4f4d585fe07a4093
Matt Vinall <matt.vinall7@gmail.com>
2022-12-15 16:33:19 -0500

    removed empty space in className

--- src/pages/create-booking.tsx
+++ src/pages/create-booking.tsx
@@ -12,7 +12,7 @@ const cards = [
 const CreateBooking: NextPage = () => {
   return (
     <>
-      <div className="container flex flex-col items-center justify-start gap-12 px-4 py-16 ">
+      <div className="container flex flex-col items-center justify-start gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
           <span className="text-[hsl(280,100%,70%)]">Book a Service</span>
         </h1>
