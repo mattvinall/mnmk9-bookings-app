600014924eb420e5fbe4ac2800a6bef46c92182e
Matt Vinall <matt.vinall7@gmail.com>
2022-12-15 15:36:35 -0500

    footer markup added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,20 @@ import React from 'react'
 
 const Footer = () => {
 	return (
-		<footer>
-			<p>This is a footer</p>
+		<footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+			<span className="text-sm text-gray-500 sm:text-center dark:text-gray-100">© 2022 <a href="https://www.mnmk9.ca" className="hover:underline">MNMK-9</a>. All Rights Reserved.
+			</span>
+			<ul className="flex flex-wrap items-center mt-3 text-sm text-gray-100 dark:text-gray-100 sm:mt-0">
+				<li>
+					<a href="/about" className="mr-4 hover:underline md:mr-6 ">About</a>
+				</li>
+				<li>
+					<a href="https://www.mnmk9.ca" target="_blank" className="mr-4 hover:underline md:mr-6">MNMK-9 Website</a>
+				</li>
+				<li>
+					<a href="/contact" className="hover:underline">Contact</a>
+				</li>
+			</ul>
 		</footer>
 	)
 }
