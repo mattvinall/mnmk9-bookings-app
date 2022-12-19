ec89e614556fcdc38cf49e0e151a24833a38aefd
Matt Vinall <matt.vinall7@gmail.com>
2022-12-19 10:29:24 -0500

    get all users by email/id

--- src/server/trpc/router/user.ts
+++ src/server/trpc/router/user.ts
@@ -1,9 +1,19 @@
 import { z } from "zod";
 
-import { router, publicProcedure } from "../trpc";
+import { router, protectedProcedure } from "../trpc";
 
 export const userRouter = router({
-	getAllUsers: publicProcedure.query(({ ctx }) => {
+	getAllUsers: protectedProcedure.query(({ ctx }) => {
 		return ctx.prisma.user.findMany();
 	}),
+	byEmail: protectedProcedure
+		.input(z.object({ email: z.string() }))
+		.query(({ ctx, input }) => {
+		return ctx.prisma.user.findUnique({ where: { email: input?.email} })
+	}),
+	byId:protectedProcedure
+	.input(z.object({ id: z.string() }))
+	.query(({ ctx, input }) => {
+	return ctx.prisma.user.findUnique({ where: { id: input?.id} })
+	})
 })
