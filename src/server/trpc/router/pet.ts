ddeba5c389c10d348327bb45eb34da1a92b47bb1
Matt Vinall <matt.vinall7@gmail.com>
2022-12-21 12:39:56 -0500

    find all pets by referencing pet table/model

--- src/server/trpc/router/pet.ts
+++ src/server/trpc/router/pet.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
-import { router, publicProcedure } from "../trpc";
+import { router, protectedProcedure } from "../trpc";
 
 export const petRouter = router({
-	getAllPets: publicProcedure.query(({ ctx }) => {
-		return ctx.prisma.services.findMany();
+	getAllPets: protectedProcedure.query(({ ctx }) => {
+		return ctx.prisma.pet.findMany();
 	})
 });
