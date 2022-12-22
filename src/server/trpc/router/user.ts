15418762caa29003ee35bef19df280761825573e
Matt Vinall <matt.vinall7@gmail.com>
2022-12-21 20:49:57 -0500

    update to user table queries to include the pets and bookings array

--- src/server/trpc/router/user.ts
+++ src/server/trpc/router/user.ts
@@ -4,16 +4,21 @@ import { router, protectedProcedure } from "../trpc";
 
 export const userRouter = router({
 	getAllUsers: protectedProcedure.query(({ ctx }) => {
-		return ctx.prisma.user.findMany();
+		return ctx.prisma.user.findMany({
+			include: {
+				pets: true,
+				bookings: true
+			}
+		});
 	}),
 	byEmail: protectedProcedure
 		.input(z.object({ email: z.string() }))
 		.query(({ ctx, input }) => {
-		return ctx.prisma.user.findUnique({ where: { email: input?.email} })
+			return ctx.prisma.user.findUnique({ where: { email: input?.email}, include: { pets: true, bookings: true,} })
 	}),
-	byId:protectedProcedure
-	.input(z.object({ id: z.string() }))
-	.query(({ ctx, input }) => {
-	return ctx.prisma.user.findUnique({ where: { id: input?.id} })
-	})
+	byId: protectedProcedure
+		.input(z.object({ id: z.string() }))
+		.query(({ ctx, input }) => {
+			return ctx.prisma.user.findUnique({ where: { id: input?.id} })
+	}),
 })
