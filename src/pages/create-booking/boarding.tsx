e3c6dac6d611d191c4b951fbbdf0179149b8a3ea
Matt Vinall <matt.vinall7@gmail.com>
2022-12-21 20:49:39 -0500

    added logic to get users pets from DB to render a dropdown

--- src/pages/create-booking/boarding.tsx
+++ src/pages/create-booking/boarding.tsx
@@ -1,24 +1,26 @@
-import React from 'react'
+import { useEffect } from "react";
 import { type NextPage } from "next";
+import { useSession } from 'next-auth/react';
 import { useForm } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
+import { trpc } from '../../utils/trpc';
 
 
-const schema = z.object({
-	name: z.string(),
-	checkInDate: z.date(),
-	checkoutDate: z.date(),
-})
+// const schema = z.object({
+// 	name: z.string(),
+// 	checkInDate: z.date(),
+// 	checkoutDate: z.date(),
+// })
+
 
 const Boarding: NextPage = () => {
 
+	const { data: sessionData } = useSession();
 
-	const { register, handleSubmit, watch, formState: { errors } } = useForm({
-		resolver: zodResolver(schema)
-	});
-
-	// const onSubmit = data => console.log(data);
+	const email = sessionData?.user?.email
+	const userByEmail = email && trpc.user.byEmail.useQuery({ email })
+	const userPets = userByEmail && userByEmail?.data?.pets;
 
 	return (
 		<div className="container flex flex-col items-center justify-start gap-12 px-4 py-16">
@@ -36,10 +38,6 @@ const Boarding: NextPage = () => {
 						<input type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-gray-100 focus:outline-none focus:ring-0 focus:border-gray-100 peer" placeholder=" " required />
 						<label htmlFor="floating_last_name" className="peer-focus:font-medium absolute text-sm text-gray-100 dark:text-gray-100 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-gray-100 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
 					</div>
-				</div>
-				<div className="relative z-0 mb-6 w-full group">
-					<input type="text" name="petName" id="petName" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-gray-100 focus:outline-none focus:ring-0 focus:border-gray-100 peer" placeholder=" " required />
-					<label htmlFor="petName" className="peer-focus:font-medium absolute text-sm text-gray-100 dark:text-gray-100 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-gray-100 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Pet Name</label>
 				</div>
 				<div className="grid md:grid-cols-2 md:gap-6">
 					<div className="relative z-0 mb-6 w-full group">
@@ -61,9 +59,24 @@ const Boarding: NextPage = () => {
 						<label htmlFor="checkOutDate" className="peer-focus:font-medium absolute text-sm text-gray-100 dark:text-gray-100 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-gray-100 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Check Out Date</label>
 					</div>
 				</div>
-				<div className="relative z-0 mb-6 w-full group">
-					<textarea type="textarea" rows="3" name="notes" id="notes" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-gray-100 focus:outline-none focus:ring-0 focus:border-gray-100 peer" placeholder=" " required />
-					<label htmlFor="notes" className="peer-focus:font-medium absolute text-sm text-gray-100 dark:text-gray-100 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-gray-100 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Notes/Special Instructions</label>
+				<div className="grid md:grid-cols-2 md:gap-6">
+					<div className="relative z-0 mb-6 w-full group">
+						{/* <input type="text" name="petName" id="petName" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-gray-100 focus:outline-none focus:ring-0 focus:border-gray-100 peer" placeholder=" " required /> */}
+						<label htmlFor="petName" className="peer-focus:font-medium absolute text-sm text-gray-100 dark:text-gray-100 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-gray-100 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Select Pet</label>
+						<select className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-gray-100 focus:outline-none focus:ring-0 focus:border-gray-100 peer">
+							{userPets && userPets.map((pet) => {
+								const { name } = pet;
+								return (
+									<option className="text-gray-900 w-[10%]" value={name}>{name}</option>
+								)
+							})}
+							<svg class="ml-2 w-4 h-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
+						</select>
+					</div>
+					<div className="relative z-0 mb-6 w-full group">
+						<textarea type="textarea" rows="1" name="notes" id="notes" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-gray-100 focus:outline-none focus:ring-0 focus:border-gray-100 peer" placeholder=" " required />
+						<label htmlFor="notes" className="peer-focus:font-medium absolute text-sm text-gray-100 dark:text-gray-100 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-gray-100 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Notes/Special Instructions</label>
+					</div>
 				</div>
 
 				<button type="submit" className="rounded-full bg-gradient-to-l from-[#667eea] to-[#764ba2] hover:bg-gradient-to-r from-[#764ba2] to-[#667eea] px-16 py-3 font-semibold text-white no-underline transition py-3 px-5 text-sm font-medium text-center rounded-lg bg--700 sm:w-fit focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Submit</button>
