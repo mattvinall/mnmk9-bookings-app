cd35fe77f286e0efd91cc6d2f69a5a05982bba1a
Matt Vinall <matt.vinall7@gmail.com>
2022-12-25 12:09:25 -0500

    added async and await for queries and wrapped logic in try/catch

--- src/server/trpc/router/user.ts
+++ src/server/trpc/router/user.ts
@@ -3,22 +3,36 @@ import { z } from "zod";
 import { router, protectedProcedure } from "../trpc";
 
 export const userRouter = router({
-	getAllUsers: protectedProcedure.query(({ ctx }) => {
-		return ctx.prisma.user.findMany({
-			include: {
-				pets: true,
-				bookings: true
-			}
-		});
+	getAllUsers: protectedProcedure.query(async ({ ctx }) => {
+		try {
+			return await ctx.prisma.user.findMany({
+				include: {
+					pets: true,
+					bookings: true
+				}
+			});
+		} catch (err) {
+			console.log(`All users cannot be fetched by email: ${err}`)
+		}
 	}),
 	byEmail: protectedProcedure
 		.input(z.object({ email: z.string() }))
-		.query(({ ctx, input }) => {
-			return ctx.prisma.user.findUnique({ where: { email: input?.email}, include: { pets: true, bookings: true,} })
+		.query(async ({ ctx, input }) => {
+			try {
+				const { email } = input;
+				return await ctx.prisma.user.findUnique({ where: { email }, include: { pets: true, bookings: true,} })
+			} catch (err) {
+				console.log(`User cannot be fetched by email: ${err}`)
+			}
 	}),
 	byId: protectedProcedure
 		.input(z.object({ id: z.string() }))
-		.query(({ ctx, input }) => {
-			return ctx.prisma.user.findUnique({ where: { id: input?.id} })
+		.query(async ({ ctx, input }) => {
+			try {
+				const { id } = input;
+				return await ctx.prisma.user.findUnique({ where: { id } })
+			} catch (err) {
+				console.log(`User cannot be fetched by ID: ${err}`)
+			}
 	}),
 })
