e05d77dd75ded69474613b641cd19fa551c63c83
Matt Vinall <matt.vinall7@gmail.com>
2022-12-25 13:34:28 -0500

    removed commented out code, added pet-select in htmlFor and matched the select id

--- src/pages/create-booking/boarding.tsx
+++ src/pages/create-booking/boarding.tsx
@@ -155,9 +155,16 @@ const Boarding: NextPage = () => {
 				</div>
 				<div className="grid md:grid-cols-2 md:gap-6">
 					<div className="relative z-0 mb-6 w-full group">
-						{/* <input type="text" name="petName" id="petName" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-gray-100 focus:outline-none focus:ring-0 focus:border-gray-100 peer"  required /> */}
-						<label htmlFor="petName" className="peer-focus:font-medium absolute text-sm text-gray-100 dark:text-gray-100 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-gray-100 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Select Pet</label>
-						<select className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-gray-100 focus:outline-none focus:ring-0 focus:border-gray-100 peer">
+						<label
+							htmlFor="pet-select"
+							className="peer-focus:font-medium absolute text-sm text-gray-100 dark:text-gray-100 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-gray-100 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+							Select Pet
+						</label>
+						<select
+							{...register("pet", { required: true })}
+							className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-gray-100 focus:outline-none focus:ring-0 focus:border-gray-100 peer"
+							id="pet-select"
+						>
 							{data?.pets && data.pets.map((pet) => {
 								const { name } = pet;
 								return (
