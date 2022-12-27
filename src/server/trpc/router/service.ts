dc1753aa9af92211dfa375aa3f4c122e7e12f2e8
Matt Vinall <matt.vinall7@gmail.com>
2022-12-27 14:59:30 -0500

    update to protectedProcedure, needs to be authenticated

--- src/server/trpc/router/service.ts
+++ src/server/trpc/router/service.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
-import { router, publicProcedure } from "../trpc";
+import { router, protectedProcedure } from "../trpc";
 
 export const serviceRouter = router({
-	getAllServices: publicProcedure.query(({ ctx }) => {
+	getAllServices: protectedProcedure.query(({ ctx }) => {
 		return ctx.prisma.services.findMany();
 	})
 });
