048785deb2ac14fdd9b32da132845c0d736d1d70
Matt Vinall <matt.vinall7@gmail.com>
2023-01-03 12:41:00 -0500

    added types to props to update setter function

--- src/components/UserDetailForm.tsx
+++ src/components/UserDetailForm.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
+import { trpc } from "../utils/trpc";
+import { useSession } from "next-auth/react";
 
-const UserDetailForm = () => {
+type Props = {
+	setShowForm: (bool: boolean) => void;
+}
+const UserDetailForm = ({ setShowForm }: Props) => {
 	type UserFormSchema = {
 		address: string,
 		city: string,
@@ -22,8 +27,20 @@ const UserDetailForm = () => {
 		resolver: zodResolver(schema)
 	});
 
+	const { data: sessionData } = useSession()
+	const id = sessionData?.user?.id;
+	const { data: userData } = trpc.user.byId.useQuery({ id });
+
+	const editProfile = trpc.user.editProfile.useMutation();
+
 	const onSubmit: SubmitHandler<UserFormSchema> = async (formData: any) => {
-		console.log("form data", formData);
+		formData.id = userData?.id;
+
+		editProfile.mutate(formData);
+
+		reset();
+
+		setShowForm(false);
 	}
 
 	return (
@@ -92,6 +109,12 @@ const UserDetailForm = () => {
 					</label>
 				</div>
 			</div>
+			<button
+				disabled={isSubmitting}
+				type="submit"
+				className="mt-[25px] rounded-full bg-gradient-to-l from-[#667eea] to-[#764ba2] hover:bg-gradient-to-r from-[#764ba2] to-[#667eea] px-16 py-3 font-semibold text-white no-underline transition py-3 px-5 text-sm font-medium text-center rounded-lg bg--700 sm:w-fit focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+				Submit
+			</button>
 		</form>
 	)
 }
