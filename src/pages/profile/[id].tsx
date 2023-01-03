a333a60d6f77b071e46ae5295aaba88de9667059
Matt Vinall <matt.vinall7@gmail.com>
2023-01-03 12:42:24 -0500

    removed imports, typescript void instead of NextPage

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -1,14 +1,9 @@
 import { useState, useEffect } from "react";
-import { type NextPage } from "next";
 import { useRouter } from "next/router";
 import UserDetailForm from "../../components/UserDetailForm";
-import Link from "next/link";
 import { trpc } from "../../utils/trpc";
-import { useForm, SubmitHandler } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from '@hookform/resolvers/zod';
 
-const UserDetail: NextPage = () => {
+const UserDetail: () => void = () => {
 	const router = useRouter();
 	const userId = router.query.id as string;
 	const { data: userDetail, isLoading, error } = trpc.user.byId.useQuery({ id: userId });
@@ -37,12 +32,14 @@ const UserDetail: NextPage = () => {
 			<h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
 				Manage  <span className="text-[hsl(280,100%,70%)]">Profile</span>
 			</h1>
-			<h2></h2>
+			<p className="text-white text-center w-[80%] font-bold sm:text-[2.5rem]">
+				Manage Profile or Add Pets to your Profile
+			</p>
 			<div className="flex justify-center">
 				<button onClick={handleShowForm} className="mt-[25px] mx-6 rounded-full bg-gradient-to-l from-[#667eea] to-[#764ba2] hover:bg-gradient-to-r from-[#764ba2] to-[#667eea] px-16 py-3 font-semibold text-white no-underline transition py-3 px-5 text-sm font-medium text-center rounded-lg bg--700 sm:w-fit focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Manage Profile</button>
 				<button onClick={handleShowModal} className="mt-[25px] mx-6 rounded-full bg-gradient-to-l from-[#667eea] to-[#764ba2] hover:bg-gradient-to-r from-[#764ba2] to-[#667eea] px-16 py-3 font-semibold text-white no-underline transition py-3 px-5 text-sm font-medium text-center rounded-lg bg--700 sm:w-fit focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Add Pet(s)</button>
 			</div>
-			<>{showForm && (<UserDetailForm />)}</>
+			<>{showForm && (<UserDetailForm setShowForm={setShowForm} />)}</>
 		</div>
 	)
 }
