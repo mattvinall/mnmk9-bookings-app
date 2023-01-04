0ccc00f5984c8e32e221fde68efe29816fd20c49
Matt Vinall <matt.vinall7@gmail.com>
2023-01-04 16:59:40 -0500

    id as string to resolve typescript error

--- src/pages/create-booking/training.tsx
+++ src/pages/create-booking/training.tsx
@@ -41,7 +41,7 @@ const schema = z.object({
 const Training: NextPage = () => {
 	// get email from session data
 	const { data: sessionData } = useSession();
-	const id = sessionData?.user?.id;
+	const id = sessionData?.user?.id as string;
 
 	// query user table by email to get user data
 	const { data, isLoading, error } = trpc.user.byId.useQuery({ id })
