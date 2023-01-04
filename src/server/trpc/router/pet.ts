8552efdcd74e36c0c6cd04e6b2c0da25895eaf1c
Matt Vinall <matt.vinall7@gmail.com>
2023-01-04 17:27:32 -0500

    add pet procedure

--- src/server/trpc/router/pet.ts
+++ src/server/trpc/router/pet.ts
@@ -7,13 +7,37 @@ export const petRouter = router({
 		return ctx.prisma.pet.findMany();
 	}),
 	byId: protectedProcedure
-	.input(z.object({ id: z.string() }))
-		.query(async ({ ctx, input }) => {
+		.input(z.object({ id: z.string() }))
+			.query(async ({ ctx, input }) => {
+				try {
+					const { id } = input;
+					return await ctx.prisma.pet.findMany({ where: { ownerId: id } })
+			} catch (err) {
+				console.log(`Pet cannot be fetched by ID: ${err}`)
+			}
+	}),
+	addPet: protectedProcedure
+		.input(
+			z.object({
+				name: z.string(),
+				ownerId: z.string(),
+				breed: z.string(),
+				vaccinated: z.boolean()
+			})
+		)
+		.mutation(async ({ ctx, input }) => {
+			const { ownerId, name, breed, vaccinated } = input;
 			try {
-				const { id } = input;
-				return await ctx.prisma.pet.findMany({ where: { ownerId: id } })
-		} catch (err) {
-			console.log(`Pet cannot be fetched by ID: ${err}`)
-		}
-}),
+				return await ctx.prisma.pet.create({
+					data: {
+						ownerId,
+						name, 
+						breed, 
+						vaccinated
+					}
+				})
+			} catch (error) {
+				console.log(`Pet cannot be created: ${error}`);
+			}
+		})
 });
