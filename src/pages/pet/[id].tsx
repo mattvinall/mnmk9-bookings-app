4fbab21e37c1610e83c95d3584106b3ca0918112
Matt Vinall <matt.vinall7@gmail.com>
2023-01-09 19:59:05 -0500

    pet details preliminary logic - need to update

--- src/pages/pet/[id].tsx
+++ src/pages/pet/[id].tsx
@@ -9,6 +9,33 @@ const PetDetail = () => {
 
 	console.log("pet detail", petDetail);
 
+	if (isLoading) return <h1 className="gap-12 px-4 py-16 text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+		Loading...
+	</h1>
+
+	if (error) return router.back();
+
+	return (
+		<div className="container flex flex-col items-center justify-start gap-12 px-4 py-16">
+			<div className="grid grid-cols-1 gap-4 lg:grid-cols-4 md:grid-cols-2 md:gap-8 mt-10">
+				{petDetail?.map((pet, i) => {
+					return (
+						<div className="flex justify-center">
+							<div className="rounded-lg shadow-lg bg-white max-w-md">
+								<img className="rounded-t-lg" src={`https://mdbootstrap.com/img/new/standard/nature/18${i}.jpg`} alt="" />
+								<div className="p-6">
+									<h2 className="text-gray-900 text-xl font-medium mb-2">{pet.name}</h2>
+									<p className="text-gray-700 text-base mb-4">{pet.breed}</p>
+									<p className="text-gray-600 font-bold text-xs">Vaccinated: {pet?.vaccinated === false ? "No" : "Yes"}</p>
+								</div>
+							</div>
+						</div>
+					)
+				})}
+			</div>
+		</div>
+	)
+
 }
 
 export default PetDetail
