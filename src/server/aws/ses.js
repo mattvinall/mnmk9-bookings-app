69a1933562837af51026dd91ff5f585f2a89eee8
Matt Vinall <matt.vinall7@gmail.com>
2023-01-10 22:40:31 -0500

    file renamed to index

--- src/server/aws/ses.js
+++
@@ -1,7 +0,0 @@
-import AWS from "aws-sdk";
-
-export const ses = new AWS.SES({
-  accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
-  region: process.env.NEXT_PUBLIC_AWS_REGION,
-});
