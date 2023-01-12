531b6cf01db538540e0c328bffd6af32c0ebaf84
Matt Vinall <matt.vinall7@gmail.com>
2023-01-12 13:35:33 -0500

    sweet alert fun

--- src/pages/manage-booking/[id].tsx
+++ src/pages/manage-booking/[id].tsx
@@ -6,25 +6,14 @@ import { trpc } from "../../utils/trpc";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from '@hookform/resolvers/zod';
+import Swal from "sweetalert2";
 
 const BookingDetail: NextPage = () => {
 	const router = useRouter();
 	const bookingId = router.query.id as string;
-	const { data: bookingDetail, isLoading, error } = trpc.bookings.byId.useQuery({ id: bookingId });
+	const { data: bookingDetail, isLoading, error, refetch } = trpc.bookings.byId.useQuery({ id: bookingId });
 
 	console.log("booking detail", bookingDetail);
-
-	const [data, setData] = useState({
-		checkInDate: bookingDetail?.checkInDate,
-		checkOutDate: bookingDetail?.checkOutDate,
-		startTime: bookingDetail?.startTime,
-		endTime: bookingDetail?.endTime,
-		notes: bookingDetail?.notes,
-	});
-
-	useEffect(() => {
-		setData(data);
-	}, []);
 
 	type FormSchemaType = {
 		checkInDate?: string,
@@ -53,10 +42,32 @@ const BookingDetail: NextPage = () => {
 
 	const onSubmit: SubmitHandler<FormSchemaType> = async (formData: any) => {
 
-		// mutate / POST request to bookings api endpoint and submit the form data
-		formData.id = bookingDetail?.id;
+		try {
 
-		editBooking.mutate(formData);
+			Swal.fire({
+				title: 'Are you sure you want to Edit?',
+				showDenyButton: true,
+				confirmButtonText: 'Yes',
+			}).then((result) => {
+				if (result.isConfirmed) {
+					// mutate / POST request to bookings api endpoint and submit the form data
+					formData.id = bookingDetail?.id;
+
+					editBooking.mutate(formData);
+
+					Swal.fire('Successfully Edited Your Booking', '', 'success');
+
+					router.push("/manage-booking")
+				}
+			})
+		} catch (error) {
+			// error message
+			Swal.fire({
+				icon: 'error',
+				title: 'Oops...',
+				text: `Something went wrong! ${error}`,
+			});
+		}
 
 		// reset the form state
 		reset();
@@ -71,14 +82,29 @@ const BookingDetail: NextPage = () => {
 
 	const handleCancelBooking = (id: String) => {
 		try {
-			const bookingId = id as string;
-			cancelBooking.mutate({ id: bookingId });
+			Swal.fire({
+				title: 'Are you sure you want to cancel?',
+				showDenyButton: true,
+				confirmButtonText: 'Yes',
+			}).then((result) => {
+				if (result.isConfirmed) {
+					const bookingId = id as string;
+					cancelBooking.mutate({ id: bookingId });
 
-			router.back()
+					Swal.fire('Cancelled Booking', '', 'success');
 
+					// redirect to home page after cancelling
+					router.push("/");
+				}
+			})
 			console.log("cancel booking", cancelBooking);
 		} catch (error) {
-			console.log("error cancelling booking", error)
+			// error message
+			Swal.fire({
+				icon: 'error',
+				title: 'Oops...',
+				text: `Something went wrong! ${error}`,
+			});
 		}
 	}
 
@@ -101,12 +127,12 @@ const BookingDetail: NextPage = () => {
 			<div className="flex justify-center">
 				<button
 					onClick={handleEditBooking}
-					className="mt-[25px] rounded-full bg-gradient-to-l from-[#667eea] to-[#764ba2] hover:bg-gradient-to-r from-[#764ba2] to-[#667eea] px-16 py-3 font-semibold text-white no-underline transition py-3 px-5 text-sm font-medium text-center rounded-lg bg--700 sm:w-fit focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+					className="mt-[25px] mr-3 rounded-full bg-gradient-to-l from-[#667eea] to-[#764ba2] hover:bg-gradient-to-r from-[#764ba2] to-[#667eea] px-16 py-3 font-semibold text-white no-underline transition py-3 px-5 text-sm font-medium text-center rounded-lg bg--700 sm:w-fit focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
 					Edit Booking
 				</button>
 				<button
 					onClick={() => handleCancelBooking(bookingDetail?.id || "")}
-					className="mt-[25px] rounded-full bg-gradient-to-l from-[#667eea] to-[#764ba2] hover:bg-gradient-to-r from-[#764ba2] to-[#667eea] px-16 py-3 font-semibold text-white no-underline transition py-3 px-5 text-sm font-medium text-center rounded-lg bg--700 sm:w-fit focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+					className="mt-[25px] ml-3 rounded-full bg-gradient-to-l from-[#667eea] to-[#764ba2] hover:bg-gradient-to-r from-[#764ba2] to-[#667eea] px-16 py-3 font-semibold text-white no-underline transition py-3 px-5 text-sm font-medium text-center rounded-lg bg--700 sm:w-fit focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
 					Cancel Booking
 				</button>
 
