c5052aa425eef3340b63c4e38ba9f2310abc5550
Matt Vinall <matt.vinall7@gmail.com>
2023-01-16 13:08:56 -0500

    defined a rows variable that was type number to resolve typescript error

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -3,6 +3,7 @@ import { type NextPage } from "next";
 
 
 const ContactUs: NextPage = () => {
+	const rows = 6;
 	return (
 		<div className="container flex flex-col items-center justify-start gap-12 px-4 py-16">
 			<h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] py-16">
@@ -21,7 +22,7 @@ const ContactUs: NextPage = () => {
 					</div>
 					<div className="sm:col-span-2 relative z-0 mb-6 w-full group">
 						<label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-100 dark:text-gray-100">Your message</label>
-						<textarea id="message" rows="6" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-gray-100 focus:outline-none focus:ring-0 focus:border-gray-100 peer" placeholder="Leave a comment..."></textarea>
+						<textarea id="message" rows={rows} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-gray-100 focus:outline-none focus:ring-0 focus:border-gray-100 peer" placeholder="Leave a comment..."></textarea>
 					</div>
 					<button type="submit" className="rounded-full bg-gradient-to-l from-[#667eea] to-[#764ba2] hover:bg-gradient-to-r from-[#764ba2] to-[#667eea] px-10 py-3 font-semibold text-white no-underline transition py-3 px-5 text-sm font-medium text-center rounded-lg bg--700 sm:w-fit focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
 				</form>
