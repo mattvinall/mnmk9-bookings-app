043fa40522a4b81dc3323135862798897b927aca
Matt Vinall <matt.vinall7@gmail.com>
2023-01-16 12:02:43 -0500

    moved key to parent div, added rel attribute on a

--- src/pages/pet/[id].tsx
+++ src/pages/pet/[id].tsx
@@ -196,9 +196,9 @@ const PetDetail = () => {
 										const formattedName = fileName && fileName.split(".")[0];
 										console.log("file name in map: ", fileName)
 										return (
-											<div style={{ position: "relative" }}>
+											<div key={doc.id} style={{ position: "relative" }}>
 												<svg onClick={() => deleteVaccinationDocument.mutate({ id: doc.id })} className="w-6 h-6" fill="#fff" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style={{ cursor: "pointer", position: "absolute", right: "0px", top: "35%" }}><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>
-												<a key={doc.id} className="flex items-center" href={doc.fileName} target="_blank">
+												<a className="flex items-center" href={doc.fileName} rel="norefferer" target="_blank">
 													<svg className="w-[80px]" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
 														<path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"></path>
 													</svg>
