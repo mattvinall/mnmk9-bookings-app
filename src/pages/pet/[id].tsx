117f3c6a1046d15250fc647644000833153ecb08
Matt Vinall <matt.vinall7@gmail.com>
2023-01-18 14:55:12 -0500

    update to file name - remove the %20 charcters from the name of the document which represents white space

--- src/pages/pet/[id].tsx
+++ src/pages/pet/[id].tsx
@@ -108,9 +108,11 @@ const PetDetail = () => {
 									</form>
 									{imageFileNamePreview && <p className="font-medium">Image Selected: {imageFileNamePreview}. <br />Click Upload to set this image.</p>}
 									{pet?.documents && pet?.documents.length > 0 && <h2 className="text-gray-900 text-xl font-medium mb-2 mt-6">Documents</h2>}
-									{pet?.documents?.map(doc => {
+									{pet.documents?.map(doc => {
+										// definte file name based on position in the array of the file name in S3
 										const fileName = doc.fileName.split("/")[5];
-										const formattedName = fileName && fileName.split(".")[0]?.replace(/^a-zA-Z0-9 ]/g, '');
+										// split the array by perdiod and return the first item in the array --> ["murphy-vaccination", ".pdf"]
+										const formattedName = fileName && fileName?.split(".")[0]?.replace(/%20/g, " ");
 										return (
 											<div key={doc.id} style={{ position: "relative" }}>
 												<svg onClick={() => deleteVaccinationDocument.mutate({ id: doc.id })} className="w-6 h-6" fill="#fff" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style={{ cursor: "pointer", position: "absolute", right: "0px", top: "35%" }}><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>
