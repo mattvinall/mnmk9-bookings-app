8ae6d744d2db2a2b0c6d34f9c0b693fb684a8f6d
Matt Vinall <matt.vinall7@gmail.com>
2023-01-19 14:56:37 -0500

    text center

--- src/pages/manage-booking/[id].tsx
+++ src/pages/manage-booking/[id].tsx
@@ -123,7 +123,7 @@ const BookingDetail: NextPage = () => {
 	return (
 		<div className="container flex flex-col items-center justify-start gap-12 px-4 py-16">
 			<Link className="flex justify-start text-left text-white font-bold text-2xl" href="/manage-booking">Go Back</Link>
-			<h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] py-8">
+			<h1 className="text-center text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] py-8">
 				Manage Booking: <span className="text-[hsl(280,100%,70%)]">{bookingDetail?.serviceName}</span>
 			</h1>
 
