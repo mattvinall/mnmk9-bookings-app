ad307ff6602bab71ffa26d617d350f0198bd45c5
Matt Vinall <matt.vinall7@gmail.com>
2023-01-19 15:42:53 -0500

    only display showcase if user is logged in

--- src/components/HomeShowCase.tsx
+++ src/components/HomeShowCase.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Link from "next/link";
 import { useSession } from 'next-auth/react';
-import { trpc } from "../utils/trpc";
 
 const HomeShowCase = () => {
 	const { data: sessionData } = useSession();
@@ -11,36 +10,40 @@ const HomeShowCase = () => {
 			<h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
 				MNMK-9 <span className="text-[hsl(280,100%,70%)]">Bookings</span>
 			</h1>
-			<h2 className="text-4xl font-bold tracking-tight text-white text-center sm:text-[3rem] my-8">Your One Stop Shop to Schedule and Manage Your Bookings.</h2>
-			<div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:gap-8">
-				<Link
-					className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-					href={`/profile/${id}`}
-				>
-					<h3 className="text-2xl font-bold">Manage Profile →</h3>
-					<p className="text-lg">
-						Before booking a service, add your pet to your profile!
-					</p>
-				</Link>
-				<Link
-					className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-					href="/create-booking"
-				>
-					<h3 className="text-2xl font-bold">Create Booking →</h3>
-					<p className="text-lg">
-						Book a service with Tyler
-					</p>
-				</Link>
-				<Link
-					className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-					href="/manage-booking"
-				>
-					<h3 className="text-2xl font-bold">Manage a Booking →</h3>
-					<p className="text-lg">
-						need to change your booking date or cancel?
-					</p>
-				</Link>
-			</div>
+			<h2 className="text-12xl font-bold tracking-tight text-white text-center sm:text-[3rem] my-8">Your One Stop Shop to Schedule and Manage Your Bookings.</h2>
+			{sessionData ? (
+				<div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:gap-8">
+					<Link
+						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+						href={`/profile/${id}`}
+					>
+						<h3 className="text-2xl font-bold">Manage Profile →</h3>
+						<p className="text-lg">
+							Before booking a service, add your pet to your profile!
+						</p>
+					</Link>
+					<Link
+						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+						href="/create-booking"
+					>
+						<h3 className="text-2xl font-bold">Create Booking →</h3>
+						<p className="text-lg">
+							Book a service with Tyler
+						</p>
+					</Link>
+					<Link
+						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+						href="/manage-booking"
+					>
+						<h3 className="text-2xl font-bold">Manage a Booking →</h3>
+						<p className="text-lg">
+							need to change your booking date or cancel?
+						</p>
+					</Link>
+				</div>
+			) : (
+				<p className="text-5xl font-extrabold tracking-tight text-white text-[2rem] md:text-[3rem]">Please Login</p>
+			)}
 		</div >
 	)
 }
