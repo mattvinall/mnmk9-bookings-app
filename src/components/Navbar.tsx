0e09722561a8c283bf5ae5fb0d9025251b542671
Matt Vinall <matt.vinall7@gmail.com>
2023-01-23 14:25:07 -0500

    added close icon based on menu toggled state

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -91,17 +91,32 @@ const Navbar: React.FC = () => {
 						<AuthShowcase />
 						<div className="md:hidden flex items-center">
 							<button className="ml-6 outline-none mobile-menu-button" onClick={handleClick}>
-								<svg className=" w-8 h-8 text-gray-900 "
-									x-show="!showMenu"
-									fill="none"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-								>
-									<path d="M4 6h16M4 12h16M4 18h16"></path>
-								</svg>
+								{!menuToggled ? (
+									<svg
+										className="w-8 h-8 text-gray-900"
+										x-show="!showMenu"
+										fill="none"
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth="2"
+										viewBox="0 0 24 24"
+										stroke="currentColor"
+									>
+										<path d="M4 6h16M4 12h16M4 18h16"></path>
+									</svg>
+								) : (
+									<svg
+										className="w-8 h-8 text-gray-900 mr-4"
+										fill="none"
+										stroke="currentColor"
+										stroke-width="1.5"
+										viewBox="0 0 24 24"
+										xmlns="http://www.w3.org/2000/svg"
+										aria-hidden="true">
+										<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+									</svg>
+								)
+								}
 							</button>
 						</div>
 					</div>
