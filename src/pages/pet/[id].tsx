8f8b548a61b430e7952b78003a3271b1f20b4069
Matt Vinall <matt.vinall7@gmail.com>
2023-01-24 12:45:30 -0500

    removed height auto, object fit and object position added to image

--- src/pages/pet/[id].tsx
+++ src/pages/pet/[id].tsx
@@ -82,7 +82,7 @@ const PetDetail = () => {
 					return (
 						<div key={pet.id} className="flex justify-center">
 							<div className="rounded-lg shadow-lg bg-white max-w-full w-[300px] md:w-[32rem]">
-								<img className="w-[auto] md:w-full rounded-t-lg" style={{ height: "auto" }} src={pet.profileImage || uploadedProfileImageUrl || `https://mdbootstrap.com/img/new/standard/nature/18${i}.jpg`} width="50" alt={pet.name} />
+								<img className="w-[auto] md:w-full rounded-t-lg h-[450px] object-cover object-center" src={pet.profileImage || uploadedProfileImageUrl || `https://mdbootstrap.com/img/new/standard/nature/18${i}.jpg`} width="50" alt={pet.name} />
 								<div className="p-6">
 									<h2 className="text-gray-900 text-xl font-medium mb-2">{pet.name}</h2>
 									<p className="text-gray-700 text-base mb-4">{pet.breed}</p>
