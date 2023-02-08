46eb5f5fcf26fc140f1051c3e09f02ac9a6391f5
Matt Vinall <matt.vinall7@gmail.com>
2023-02-08 10:17:22 -0500

    added newContactEmail procedure to store the info from the form in the table

--- src/server/trpc/router/contact.ts
+++ src/server/trpc/router/contact.ts
@@ -4,29 +4,37 @@ import { z } from "zod";
 import { router, publicProcedure, protectedProcedure } from "../trpc";
 
 export const contactRouter = router({
-	getAll: protectedProcedure.query(async ({ ctx }) => {
-		// return await ctx.prisma.
+	getAll: protectedProcedure.input(
+		z.object({
+			role: z.string(),
+		})
+	)
+	.query(async ({ ctx, input }) => {
+		const  { role } = input;
+		if (role === "admin") {
+			return await ctx.prisma.contact.findMany();
+		}
 	}),
-	// newContactEmail: publicProcedure
-	// .input(
-	// 	z.object({
-	// 		name: z.string(),
-	// 		email: z.string(),
-	// 		message: z.string()
-	// 	})
-	// )
-	// .mutation(async ({ ctx, input }) => {
-	// 	try {
-	// 		const { name, email, message } = input
-	// 		return await ctx.prisma.contact.create({
-	// 			data: {
-	// 				name,
-	// 				email,
-	// 				message
-	// 			}
-	// 		})
-	// 	} catch (error) {
-	// 		console.log(`support email could not be created: ${error}`)
-	// 	}
-	// }),
+	newContactEmail: publicProcedure
+	.input(
+		z.object({
+			name: z.string(),
+			email: z.string(),
+			message: z.string()
+		})
+	)
+	.mutation(async ({ ctx, input }) => {
+		try {
+			const { name, email, message } = input
+			return await ctx.prisma.contact.create({
+				data: {
+					name,
+					email,
+					message
+				}
+			})
+		} catch (error) {
+			console.log(`support email could not be created: ${error}`)
+		}
+	}),
 });
