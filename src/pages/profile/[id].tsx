a728aef8b53e5f27c8cc29fd2964f73b84608a0c
Matt Vinall <matt.vinall7@gmail.com>
2023-02-13 14:31:52 -0500

    changed verbiage to remove instead of delete

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -12,7 +12,6 @@ const UserDetail: () => void = () => {
 	const router = useRouter();
 	const userId = router.query.id as string;
 	const { data: userDetail, isLoading, refetch, error } = trpc.user.byId.useQuery({ id: userId });
-
 	const [showUserForm, setShowUserForm] = useState<boolean>(false);
 	const [showPetForm, setShowPetForm] = useState<boolean>(false);
 
@@ -98,7 +97,7 @@ const UserDetail: () => void = () => {
 											</Link>
 											<button name="delete-pet" onClick={() => handleDeletePet(pet.id, pet.name)} className="mt-6 ml-4 flex flex-col items-center justify-center w-12 h-12 mr-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-full focus:shadow-outline hover:bg-indigo-800">
 												<svg className="w-4 h-4" fill="#fff" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
-												<p className=" text-xs text-gray-100">Delete</p>
+												<p className="text-xs text-gray-100">Remove</p>
 											</button>
 										</div>
 									</div>
