b91df627d50a500934d0e6cd124f6b254dddefcd
Matt Vinall <matt.vinall7@gmail.com>
2023-03-10 22:33:14 -0500

    added separate schemas based on the type of form/booking

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 // define schema for the form 
-export const schema = z.object({
+export const boardingSchema = z.object({
 	firstName: z.string().min(1, { message: "First name is required" }),
 	lastName: z.string().min(1, { message: "Last name is required" }),
 	phoneNumber: z.string(),
@@ -9,7 +9,72 @@ export const schema = z.object({
 	}),
 	checkInDate: z.string(),
 	checkOutDate: z.string(),
+	petName: z.string(),
+	notes: z.string(),
+	petData: z.object({
+		id: z.string(),
+		breed: z.string(),
+		name: z.string(),
+		ownerId: z.string(),
+		profileImage: z.string(),
+		vaccinated: z.boolean(),
+	}).optional()
+});
+
+export const daycareSchema = z.object({
+	firstName: z.string().min(1, { message: "First name is required" }),
+	lastName: z.string().min(1, { message: "Last name is required" }),
+	phoneNumber: z.string(),
+	email: z.string().min(1, { message: "Email is required" }).email({
+		message: "Must be a valid email",
+	}),
+	checkInDate: z.string(),
 	startTime: z.string(),
+	endTime: z.string(),
+	petName: z.string(),
+	notes: z.string(),
+	petData: z.object({
+		id: z.string(),
+		breed: z.string(),
+		name: z.string(),
+		ownerId: z.string(),
+		profileImage: z.string(),
+		vaccinated: z.boolean(),
+	}).optional()
+});
+
+export const groomingSchema = z.object({
+	firstName: z.string().min(1, { message: "First name is required" }),
+	lastName: z.string().min(1, { message: "Last name is required" }),
+	phoneNumber: z.string(),
+	email: z.string().min(1, { message: "Email is required" }).email({
+		message: "Must be a valid email",
+	}),
+	checkInDate: z.string(),
+	startTime: z.string(),
+	endTime: z.string(),
+	petName: z.string(),
+	notes: z.string(),
+	petData: z.object({
+		id: z.string(),
+		breed: z.string(),
+		name: z.string(),
+		ownerId: z.string(),
+		profileImage: z.string(),
+		vaccinated: z.boolean(),
+	}).optional()
+});
+
+export const trainingSchema = z.object({
+	firstName: z.string().min(1, { message: "First name is required" }),
+	lastName: z.string().min(1, { message: "Last name is required" }),
+	phoneNumber: z.string(),
+	email: z.string().min(1, { message: "Email is required" }).email({
+		message: "Must be a valid email",
+	}),
+	checkInDate: z.string(),
+	startTime: z.string(),
+	endTime: z.string(),
 	petName: z.string(),
 	notes: z.string(),
 	petData: z.object({
