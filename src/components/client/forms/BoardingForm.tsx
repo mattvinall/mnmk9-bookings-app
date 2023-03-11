e4593b96c30f174d501b9f03a1169151ce57c884
Matt Vinall <matt.vinall7@gmail.com>
2023-03-10 22:29:36 -0500

    import Pet type from prisma

--- src/components/client/forms/BoardingForm.tsx
+++ src/components/client/forms/BoardingForm.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { trpc } from "../../../utils/trpc";
+import type { Pet } from "@prisma/client";
 
 type Props = {
-	petData: Array<{ name: string, ownerId: string }>,
+	petData: Array<Pet>,
 	isSubmitting: boolean,
 	register: any,
 	handleSubmit: any,
@@ -25,7 +26,6 @@ const BoardingForm = ({ petData, isSubmitting, register, handleSubmit, onSubmit,
 						defaultValue={userData?.name.split(" ")[0] || ""}
 						id="floating_first_name"
 						className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-gray-100 focus:outline-none focus:ring-0 focus:border-gray-100 peer"
-						required
 					/>
 					<label
 						htmlFor="floating_first_name"
@@ -38,10 +38,9 @@ const BoardingForm = ({ petData, isSubmitting, register, handleSubmit, onSubmit,
 						{...register("lastName", { required: true })}
 						type="text"
 						name="lastName"
-						defaultValue={userData?.name.split(" ")[1]}
+						defaultValue={userData?.name.split(" ")[1] || ""}
 						id="floating_last_name"
 						className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-gray-100 focus:outline-none focus:ring-0 focus:border-gray-100 peer"
-						required
 					/>
 					<label
 						htmlFor="floating_last_name"
@@ -59,7 +58,6 @@ const BoardingForm = ({ petData, isSubmitting, register, handleSubmit, onSubmit,
 						defaultValue={userData?.phoneNumber || ""}
 						id="floating_phone"
 						className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-gray-100 focus:outline-none focus:ring-0 focus:border-gray-100 peer"
-						required
 					/>
 					<label
 						htmlFor="floating_phone"
@@ -75,7 +73,6 @@ const BoardingForm = ({ petData, isSubmitting, register, handleSubmit, onSubmit,
 						defaultValue={userData?.email || ""}
 						id="floating_email"
 						className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-gray-100 focus:outline-none focus:ring-0 focus:border-gray-100 peer"
-						required
 					/>
 					<label
 						htmlFor="floating_email"
