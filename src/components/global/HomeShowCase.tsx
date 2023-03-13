983678b49a5620cb581a50bd88f814e5fdc53a76
Matt Vinall <matt.vinall7@gmail.com>
2023-03-13 11:15:54 -0400

    update to home show case - different view/links for admin vs client

--- src/components/global/HomeShowCase.tsx
+++ src/components/global/HomeShowCase.tsx
@@ -1,10 +1,81 @@
 import React from 'react'
 import Link from "next/link";
 import { useSession } from 'next-auth/react';
+import { trpc } from "../../utils/trpc";
 
 const HomeShowCase = () => {
 	const { data: sessionData } = useSession();
-	const id = sessionData?.user?.id;
+	const id = sessionData?.user?.id as string;
+
+	const { data: userData } = trpc.user.byId.useQuery({ id });
+
+	const ClientShowCase = () => {
+		return (
+			<>
+				<Link
+					className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+					href={`/profile/${id}`}
+				>
+					<h3 className="text-2xl font-bold">Manage Profile →</h3>
+					<p className="text-lg text">
+						Before booking a service, add your pet to your profile!
+					</p>
+				</Link>
+				<Link
+					className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+					href="/create-booking"
+				>
+					<h3 className="text-2xl font-bold">Create Booking →</h3>
+					<p className="text-lg">
+						Book a service with Tyler
+					</p>
+				</Link>
+				<Link
+					className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+					href="/manage-booking"
+				>
+					<h3 className="text-2xl font-bold">Manage a Booking →</h3>
+					<p className="text-lg">
+						need to change your booking date or cancel?
+					</p>
+				</Link>
+			</>
+		)
+	}
+	const AdminShowCase = () => {
+		return (
+			<>
+				<Link
+					className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+					href={`/dashboard`}
+				>
+					<h3 className="text-2xl font-bold">Dashboard →</h3>
+					<p className="text-lg text">
+						Daily metrics, calendar view, manage daily tasks and easily confirm your bookings!
+					</p>
+				</Link>
+				<Link
+					className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+					href="/users"
+				>
+					<h3 className="text-2xl font-bold">Manage Users →</h3>
+					<p className="text-lg">
+						Search and view user profiles quickly.
+					</p>
+				</Link>
+				<Link
+					className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+					href="/manage-booking"
+				>
+					<h3 className="text-2xl font-bold">Manage a Booking →</h3>
+					<p className="text-lg">
+						need to update or cancel a clients' booking?
+					</p>
+				</Link>
+			</>
+		)
+	}
+
 	return (
 		<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
 			<h1 className="text-5xl text-center font-extrabold tracking-tight text-white sm:text-[5rem]">
@@ -13,34 +84,13 @@ const HomeShowCase = () => {
 			<h2 className="text-12xl font-bold tracking-tight text-white text-center text-[2rem] sm:text-[3rem] my-8">Your One Stop Shop to Schedule and Manage Your Bookings.</h2>
 			{sessionData ? (
 				<div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:gap-8">
-					<Link
-						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-						href={`/profile/${id}`}
-					>
-						<h3 className="text-2xl font-bold">Manage Profile →</h3>
-						<p className="text-lg text">
-							Before booking a service, add your pet to your profile!
-						</p>
-					</Link>
-					<Link
-						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-						href="/create-booking"
-					>
-						<h3 className="text-2xl font-bold">Create Booking →</h3>
-						<p className="text-lg">
-							Book a service with Tyler
-						</p>
-					</Link>
-					<Link
-						className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-						href="/manage-booking"
-					>
-						<h3 className="text-2xl font-bold">Manage a Booking →</h3>
-						<p className="text-lg">
-							need to change your booking date or cancel?
-						</p>
-					</Link>
-				</div>
+					{userData?.role === "user" ? (
+						<ClientShowCase />
+					) : (
+						<AdminShowCase />
+					)
+					}
+				</div >
 			) : (
 				<p className="text-5xl font-extrabold tracking-tight text-white text-[2rem] md:text-[3rem]">Please Login</p>
 			)}
