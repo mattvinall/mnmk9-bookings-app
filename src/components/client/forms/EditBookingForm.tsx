dda14253eddcd8ddfebfb6926a06990d909b21c2
Matt Vinall <matt.vinall7@gmail.com>
2023-03-15 12:04:58 -0400

    added the booking detail card so user can reference the details to fill out form

--- src/components/client/forms/EditBookingForm.tsx
+++ src/components/client/forms/EditBookingForm.tsx
@@ -19,11 +19,24 @@ const EditBookingForm = ({ register, handleSubmit, onSubmit, isSubmitting, showF
 	return (
 		<form className="w-[80%] md:w-[90%]" style={{ position: "relative" }} onSubmit={handleSubmit(onSubmit)}>
 			<svg onClick={() => setShowForm(false)} className="w-6 h-6" fill="#fff" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style={{ cursor: "pointer", position: "absolute", right: "0px", top: "0%", color: "white" }}><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>
-			<p className="text-lg text-gray-100 underline">Details</p>
-			{bookingDetail?.checkInDate ? <p className="text-md text-gray-100">scheduled check-in date: {bookingDetail?.checkInDate}</p> : null}
-			{bookingDetail?.checkOutDate ? <p className="pb-8 text-md text-gray-100">scheduled check-out date: {bookingDetail?.checkOutDate}</p> : null}
-			{bookingDetail?.startTime ? <p className="text-md text-gray-100">scheduled start time/drop off time: {bookingDetail?.startTime}</p> : null}
-			{bookingDetail?.endTime ? <p className="pb-8 text-md text-gray-100">scheduled end time/pick up time: {bookingDetail?.endTime}</p> : null}
+			<h2 className="text-[2rem] text-white font-bold">Booking Details</h2>
+			<ul className="my-8 lg:my-8">
+				<li className="flex w-[350px] justify-center flex-col gap-4 rounded-xl bg-white/10 p-2 text-white hover:bg-white/20">
+					<div className="flex justify-center">
+						<div className="rounded-lg shadow-lg bg-white max-w-md w-full h-full min-h-[300px]">
+							<div className="p-6">
+								<span className="inline-block text-xl font-bold uppercase text-gray-900 mb-2">{bookingDetail?.serviceName} | </span><span className="inline-block text-xl font-bold uppercase text-gray-900 mb-2"> &nbsp;{bookingDetail?.petName}</span>
+								<p className="text-md text-gray-700 mb-2"><span className="font-bold">Confirmed:</span> {bookingDetail?.confirmedBooking ? "✅" : "❌"}</p>
+								<p className="text-md text-gray-700 mb-2"><span className="font-bold">Check In:</span> {bookingDetail?.checkInDate}</p>
+								<p className="text-md text-gray-700 mb-2"><span className="font-bold">Check Out:</span> {bookingDetail?.checkOutDate ? bookingDetail?.checkOutDate : "--"}</p>
+								<p className="text-md text-gray-700 mb-2"><span className="font-bold">Start Time:</span> {bookingDetail?.startTime ? (bookingDetail?.startTime) : "--"}</p>
+								<p className="text-md text-gray-700 mb-2"><span className="font-bold">End Time:</span> {bookingDetail?.endTime ? (bookingDetail?.endTime) : "--"}</p>
+								<p className="text-md text-gray-700 mb-2"><span className="font-bold">Notes:</span> {bookingDetail?.notes ? bookingDetail?.notes : "--"}</p>
+							</div>
+						</div>
+					</div>
+				</li>
+			</ul>
 			<div className={bookingDetail?.checkOutDate ? `grid md:grid-cols-2 md:gap-6` : `grid md:grid-cols-1 md:gap-6`}>
 				<div className="relative z-0 mb-6 w-full group">
 					<input
@@ -73,7 +86,7 @@ const EditBookingForm = ({ register, handleSubmit, onSubmit, isSubmitting, showF
 						<label
 							htmlFor="startTime"
 							className="peer-focus:font-medium absolute text-sm text-gray-100 dark:text-gray-100 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-gray-100 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-							Start Time / Drop off Time: {bookingDetail?.endTime}
+							Start Time / Drop off Time:
 						</label>
 					</div>
 					<div className="relative z-0 mb-6 w-full group">
@@ -88,7 +101,7 @@ const EditBookingForm = ({ register, handleSubmit, onSubmit, isSubmitting, showF
 						<label
 							htmlFor="endTime"
 							className="peer-focus:font-medium absolute text-sm text-gray-100 dark:text-gray-100 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-gray-100 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-							End Time / Pick Up Time: {bookingDetail?.endTime}
+							End Time / Pick Up Time:
 						</label>
 					</div>
 				</div>
