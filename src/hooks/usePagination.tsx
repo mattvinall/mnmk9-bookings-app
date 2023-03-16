bc8ed8326604abf65ebc06bb8cf6f3c979e93c25
Matt Vinall <matt.vinall7@gmail.com>
2023-03-15 21:16:05 -0400

    use pagination hook logic

---
+++ src/hooks/usePagination.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+const usePagination = (data: any, itemsPerPage: number) => {
+	const [currentPage, setCurrentPage] = useState(1);
+	const itemCount = data?.length;
+
+	const getCurrentData = () => {
+		const start = (currentPage - 1) * itemsPerPage;
+		const end = start + itemsPerPage;
+
+		return data?.slice(start, end);
+	};
+
+	const pageCount = Math.ceil(itemCount / itemsPerPage);
+
+	const changePage = (page: number) => {
+		setCurrentPage(page)
+	}
+
+	return {
+		currentPage, getCurrentData, setCurrentPage, pageCount, changePage
+	};
+};
+
+export default usePagination;
