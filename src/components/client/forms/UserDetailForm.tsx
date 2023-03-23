fe9f195887ceefcf58270a7834bf11cf38e553a0
Matt Vinall <matt.vinall7@gmail.com>
2023-03-23 13:51:28 -0400

    extracted schema and imported type from types file

--- src/components/client/forms/UserDetailForm.tsx
+++ src/components/client/forms/UserDetailForm.tsx
@@ -6,18 +6,12 @@ import { trpc } from "../../../utils/trpc";
 import { useSession } from "next-auth/react";
 import Swal from "sweetalert2";
 import { useRouter } from 'next/router';
+import { UserFormSchema } from '../../../types/form-shema';
 
 type Props = {
 	setShowUserForm: (bool: boolean) => void;
 }
 const UserDetailForm = ({ setShowUserForm }: Props) => {
-	type UserFormSchema = {
-		address: string,
-		city: string,
-		postalCode: string,
-		phoneNumber: string,
-	}
-
 	const schema = z.object({
 		address: z.string().min(6).max(65),
 		city: z.string().min(1),
