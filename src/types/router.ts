013ef3105a389d3b044afa08590a9936949223fa
Matt Vinall <matt.vinall7@gmail.com>
2023-03-23 13:01:10 -0400

    router types using RouterOutputs helper

---
+++ src/types/router.ts
@@ -0,0 +1,7 @@
+import { RouterOutputs } from "../utils/trpc";
+
+export type BookingsArray = RouterOutputs["bookings"]["getAllBookings"]
+export type Booking = RouterOutputs["bookings"]["getAllBookings"][number]
+
+export type PetsArray = RouterOutputs["pet"]["getAllPets"]
+export type Pet = RouterOutputs["pet"]["getAllPets"][number]
