7301b3702c8f9dd52b943d6f7b8012f00e1ff45a
Matt Vinall <matt.vinall7@gmail.com>
2023-03-27 12:37:46 -0400

    rate limiter using upstash and redis

---
+++ src/lib/rateLimit.ts
@@ -0,0 +1,9 @@
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis";
+
+// Create a new ratelimiter, that allows 10 requests per 10 seconds
+export const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true
+});
