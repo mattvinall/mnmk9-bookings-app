3336b4371314716c007b838bfd745e5fc59dabf9
Matt Vinall <matt.vinall7@gmail.com>
2023-03-30 10:07:40 -0400

    bookings calendar fix

--- src/components/admin/widgets/BookingsCalendar.tsx
+++ src/components/admin/widgets/BookingsCalendar.tsx
@@ -122,9 +122,9 @@ const BookingsCalendar = () => {
 					<Calendar className="!lg:w-full" value={date} onChange={handleDateChange} />
 					<div className="mt-5 relative">
 						{/* Check In Table */}
-						{checkInBookings.length > 0 ? <CheckInTable checkInBookings={checkInBookings} checkInBookingsList={checkInBookingsList} /> : <p className="text-[1.2rem] text-gray-100 text-left">Please click on a date to see your bookings</p>}
+						{checkInBookings && checkInBookings?.length > 0 ? <CheckInTable checkInBookings={checkInBookings} checkInBookingsList={checkInBookingsList} /> : <p className="text-[1.2rem] text-gray-100 text-left">Please click on a date to see your bookings</p>}
 						{/* Check Out Table */}
-						{checkOutBookings.length > 0 && <CheckOutTable checkOutBookings={checkOutBookings} checkOutBookingsList={checkOutBookingsList} />}
+						{checkOutBookings && checkOutBookings?.length > 0 && <CheckOutTable checkOutBookings={checkOutBookings} checkOutBookingsList={checkOutBookingsList} />}
 					</div>
 
 				</div>
