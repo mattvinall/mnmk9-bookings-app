689cf1d3a37f5908f5afaa3860ee7195fdbbef88
Matt Vinall <matt.vinall7@gmail.com>
2023-03-30 11:00:12 -0400

    heading style

--- src/pages/create-booking/grooming.tsx
+++ src/pages/create-booking/grooming.tsx
@@ -190,7 +190,7 @@ const Grooming: NextPage = () => {
 		sessionData ? (
 			<div className="container flex flex-col items-center justify-start gap-12 px-4 py-16">
 				<h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem] py-16">
-					Book <span className="text-[hsl(280,100%,70%)]">Grooming</span>
+					Book <span className="text-[rgb(103,163,161)]">Grooming</span>
 				</h1>
 
 				<p className="text-white text-center w-[80%] font-bold sm:text-[2.5rem]">
