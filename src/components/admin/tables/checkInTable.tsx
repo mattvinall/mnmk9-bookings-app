34d63db50438dfe252778017bdd663c5cb28c437
mattv993 <mattv@q4inc.com>
2023-04-04 15:44:03 -0400

    conditionally rendered bookings UI based on screen width

--- src/components/admin/tables/checkInTable.tsx
+++ src/components/admin/tables/checkInTable.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+import Media from "react-media";
 
 type Props = {
 	checkInBookings: any
@@ -8,37 +12,83 @@ type Props = {
 }
 
 const CheckInTable = ({ checkInBookings, checkInBookingsList }: Props): ReactJSXElement => {
+	const [showArrows, setShowArrows] = useState<boolean>(true);
+
+	useEffect(() => {
+		if (checkInBookings && checkInBookings.length > 1) {
+			setShowArrows(true);
+			return;
+		}
+
+		setShowArrows(false);
+	}, [showArrows, checkInBookings?.length]);
+
 	return (
 		<>
 			<h3 className="text-white font-bold text-[2rem] mb-8">Checking In:</h3>
-			<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-				<thead className={`text-md text-gray-700 uppercase bg-white dark:bg-white dark:text-navy-800`}>
-					<tr>
-						<th scope="col" className="px-6 py-3">
-							Name
-						</th>
-						<th scope="col" className="px-6 py-3">
-							Service
-						</th>
-						<th scope="col" className="px-6 py-3">
-							Date (Check In/Start)
-						</th>
-						<th scope="col" className="px-6 py-3">
-							Date (Check Out/End)
-						</th>
-						<th scope="col" className="px-6 py-3">
-							Time (Start)
-						</th>
-						<th scope="col" className="px-6 py-3">
-							Time (End)
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					{checkInBookings && checkInBookings.length > 0 ? checkInBookingsList : <p className="text-white text-[1.2rem] overflow-none mt-4">Nobody checking in this date.</p>}
-				</tbody>
-			</table>
+			<Media query={{ minWidth: 900 }}>
+				{matches => matches ? (
 
+					<table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+						<thead className={`text-md text-gray-700 uppercase bg-white dark:bg-white dark:text-navy-800`}>
+							<tr>
+								<th scope="col" className="px-6 py-3">
+									Name
+						</th>
+								<th scope="col" className="px-6 py-3">
+									Service
+						</th>
+								<th scope="col" className="px-6 py-3">
+									Date (Check In/Start)
+						</th>
+								<th scope="col" className="px-6 py-3">
+									Date (Check Out/End)
+						</th>
+								<th scope="col" className="px-6 py-3">
+									Time (Start)
+						</th>
+								<th scope="col" className="px-6 py-3">
+									Time (End)
+						</th>
+							</tr>
+						</thead>
+						<tbody>
+							{checkInBookings && checkInBookings.length > 0 ? checkInBookingsList : <p className="text-white text-[1.2rem] overflow-none mt-4">Nobody checking in this date.</p>}
+						</tbody>
+					</table>
+				) : (
+						<Splide aria-label="MNMK-9 Bookings that have not confirmed" options={{ arrows: showArrows === true ? true : false }}>
+							{checkInBookings && checkInBookings?.map((booking: any) => {
+								return (
+									<SplideSlide key={booking?.id}>
+										<li className="flex flex-col w-[300px] md:w-[400px] gap-4 rounded-xl p-2 text-white ">
+											<div className="flex justify-center">
+												<div className="rounded-lg shadow-lg bg-white max-w-md w-full h-full min-h-[320px]">
+													<div className="bg-gray shadow-lg  overflow-hidden w-full max-w-md">
+														<div className="bg-gray-700 px-4 py-2">
+															<h2 className="uppercase text-lg font-bold">{booking.firstName} {booking.lastName}</h2>
+															<a href={`mailto:${booking.email}`} className="inline-blocktext-sm mb-2 hover:underline">{booking.email}</a>
+															<a href={`tel:${booking.phoneNumber}`} className="block text-sm mb-2 hover:underline">{booking.phoneNumber}</a>
+														</div>
+													</div>
+													<div className="p-6">
+														<span className="inline-block text-xl font-bold uppercase text-gray-900 mb-2">{booking.serviceName} | </span><span className="inline-block text-xl font-bold uppercase text-gray-900 mb-2"> &nbsp;{booking.petName}</span>
+														<p className="text-md text-gray-700 mb-2"><span className="font-bold">Confirmed:</span> {booking.confirmedBooking ? "✅" : "❌"}</p>
+														<p className="text-md text-gray-700 mb-2"><span className="font-bold">Check In:</span> {booking.checkInDate}</p>
+														<p className="text-md text-gray-700 mb-2"><span className="font-bold">Check Out:</span> {booking.checkOutDate ? booking.checkOutDate : "--"}</p>
+														<p className="text-md text-gray-700 mb-2"><span className="font-bold">Start Time:</span> {booking.startTime ? (booking.startTime) : "--"}</p>
+														<p className="text-md text-gray-700 mb-2"><span className="font-bold">End Time:</span> {booking.endTime ? (booking.endTime) : "--"}</p>
+														<p className="text-md text-gray-700 mb-2"><span className="font-bold">Notes:</span> {booking?.notes ? booking.notes : "--"}</p>
+													</div>
+												</div>
+											</div>
+										</li>
+									</SplideSlide>
+								)
+							})}
+						</Splide>
+					)}
+			</Media>
 		</>
 	)
 }
