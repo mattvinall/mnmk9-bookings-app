9c49332f05952c8753d6576c0506cd6174a703a9
Matt Vinall <matt.vinall7@gmail.com>
2023-05-25 13:07:33 -0400

    added a cache file to export client and getter/setter functions

---
+++ src/lib/cache.ts
@@ -0,0 +1,14 @@
+import Redis from "ioredis";
+import { env } from "../env/server.mjs";
+
+export const client = new Redis(env.REDIS_DB_URL);
+
+export const getCache = async (key: string) => {
+  const cache = await client.get(key);
+  return cache ? JSON.parse(cache) : null;
+};
+
+export const setCache = async (key: string, data: [] | {} | null) => {
+  await client.set(key, JSON.stringify(data));
+  await client.expire(key, 1000);
+};
