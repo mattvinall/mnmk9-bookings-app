9c0df49f2f10e12311499b3eb8f752920e5fdce6
Matt Vinall <matt.vinall7@gmail.com>
2023-05-24 20:29:12 -0400

    refactor to include functions for getting and setting cache to improve performance

--- src/server/trpc/router/user.ts
+++ src/server/trpc/router/user.ts
@@ -1,120 +1,166 @@
-import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-
 import { router, protectedProcedure } from "../trpc";
+import Redis from "ioredis";
+import { env } from "../../../env/server.mjs";
+
+const client = new Redis(env.REDIS_DB_URL);
+
+const getUserCache = async (key: string) => {
+  const cache = await client.get(key);
+  return cache ? JSON.parse(cache) : null;
+};
+
+const setUserCache = async (key: string, data: [] | {} | null) => {
+  await client.set(key, JSON.stringify(data));
+  await client.expire(key, 1000);
+};
 
 export const userRouter = router({
-	getAllUsers: protectedProcedure.query(async ({ ctx }) => {
-		try {
-			return await ctx.prisma.user.findMany({
-				include: {
-					pets: true,
-					bookings: true
-				}
-			});
-		} catch (err) {
-			console.log(`All users cannot be fetched by email: ${err}`)
-		}
-	}),
-	getRoleById: protectedProcedure
-		.input(
-			z.object({
-				id: z.string()
-			})
-		)
-		.query(async ({ ctx, input }) => {
-			const { id } = input
-			return await ctx.prisma.user.findUnique({
-				where: { id },
-				select: {
-					role: true
-				}
-			});
-		}),
-	byEmail: protectedProcedure
-		.input(z.object({ email: z.string() }))
-		.query(async ({ ctx, input }) => {
-			try {
-				const { email } = input;
-				return await ctx.prisma.user.findUnique({ where: { email }, include: { pets: true, bookings: true,} })
-			} catch (err) {
-				console.log(`User cannot be fetched by email: ${err}`)
-			}
-	}),
-	byId: protectedProcedure
-		.input(z.object({ id: z.string() }))
-		.query(async ({ ctx, input }) => {
-			try {
-				const { id } = input;
-				if (!id) {
-					throw new TRPCError({code: "BAD_REQUEST"});	
-				}
-				
-				return await ctx.prisma.user.findUnique({ where: { id }, include: { pets: true, bookings: true}})
-			} catch (err) {
-				console.log(`User cannot be fetched by ID: ${err}`)
-			}
-		}),
-	editProfile: protectedProcedure
-		.input(
-			z.object({
-				id: z.string(),
-				address: z.string().optional(),
-				city: z.string().optional(),
-				postalCode: z.string().optional(),
-				phoneNumber: z.string().optional()
-			})
-		)
-		.mutation(async ({ ctx, input }) => {
-			const { id, address, city, postalCode, phoneNumber } = input;
-			return await ctx.prisma.user.update({ 
-				where: {
-					id
-				},
-				data: {
-					address,
-					city,
-					postalCode,
-					phoneNumber
-				}
-		})
-	}),
-	makeUserAdmin: protectedProcedure
-	.input(
-		z.object({
-			id: z.string()
-		})
-	)
-	.mutation(async ({ ctx, input }) => {
-		const { id } = input;
-		try {
-			return await ctx.prisma.user.update({
-				where: { id },
-				data: {
-					role: "admin"
-				}
-			})
-		} catch (error) {
-			console.log(`error: could not update user role - ${error}`)
-		}
-	}),
-	removeUserAdmin: protectedProcedure
-	.input(
-		z.object({
-			id: z.string()
-		})
-	)
-	.mutation(async ({ ctx, input }) => {
-		const { id } = input;
-		try {
-			return await ctx.prisma.user.update({
-				where: { id },
-				data: {
-					role: "user"
-				}
-			})
-		} catch (error) {
-			console.log(`error: could not update user role - ${error}`)
-		}
-	})
-})
+  getAllUsers: protectedProcedure.query(async ({ ctx }) => {
+    try {
+      const cache = await getUserCache("allUsers");
+      if (cache) {
+        return cache;
+      } else {
+        const allUsers = await ctx.prisma.user.findMany({
+          include: {
+            pets: true,
+            bookings: true
+          }
+        });
+        await setUserCache("allUsers", allUsers);
+        return allUsers;
+      }
+    } catch (err) {
+      console.log(`Error fetching all users: ${err}`);
+    }
+  }),
+
+  getRoleById: protectedProcedure
+    .input(
+      z.object({
+        id: z.string()
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      try {
+        const cache = await getUserCache("userByRole");
+        if (cache) {
+          console.log("Cache successfully retrieved: ", cache);
+          return cache;
+        } else {
+          const { id } = input;
+          const userByRole = await ctx.prisma.user.findUnique({
+            where: { id },
+            select: {
+              role: true
+            }
+          });
+          await setUserCache("userByRole", userByRole);
+          return userByRole;
+        }
+      } catch (err) {
+        console.log(`Error fetching user by role: ${err}`);
+      }
+    }),
+
+  byEmail: protectedProcedure
+    .input(z.object({ email: z.string() }))
+    .query(async ({ ctx, input }) => {
+      try {
+        const { email } = input;
+        return await ctx.prisma.user.findUnique({
+          where: { email },
+          include: { pets: true, bookings: true }
+        });
+      } catch (err) {
+        console.log(`Error fetching user by email: ${err}`);
+      }
+    }),
+
+  byId: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      try {
+        const cache = await getUserCache("user");
+        if (cache) {
+          console.log("Cache successfully retrieved: ", cache);
+          return cache;
+        } else {
+          const user = await ctx.prisma.user.findUnique({
+            where: { id: input?.id },
+            include: { pets: true, bookings: true }
+          });
+          await setUserCache("user", user);
+          return user;
+        }
+      } catch (err) {
+        console.log(`Error fetching user by ID: ${err}`);
+      }
+    }),
+
+  editProfile: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        address: z.string().optional(),
+        city: z.string().optional(),
+        postalCode: z.string().optional(),
+        phoneNumber: z.string().optional()
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id, address, city, postalCode, phoneNumber } = input;
+      return await ctx.prisma.user.update({
+        where: {
+          id
+        },
+        data: {
+          address,
+          city,
+          postalCode,
+          phoneNumber
+        }
+      });
+    }),
+
+  makeUserAdmin: protectedProcedure
+    .input(
+      z.object({
+        id: z.string()
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id } = input;
+      try {
+        return await ctx.prisma.user.update({
+          where: { id },
+          data: {
+            role: "admin"
+          }
+        });
+      } catch (error) {
+        console.log(`Error updating user role: ${error}`);
+      }
+    }),
+
+  removeUserAdmin: protectedProcedure
+    .input(
+      z.object({
+        id: z.string()
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id } = input;
+      try {
+        return await ctx.prisma.user.update({
+          where: { id },
+          data: {
+            role: "user"
+          }
+        });
+      } catch (error) {
+        console.log(`Error updating user role: ${error}`);
+      }
+    })
+});
