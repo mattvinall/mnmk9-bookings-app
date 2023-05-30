021b4ce30baf6d2dfcb1bf0ee569006e6770f222
Matt Vinall <matt.vinall7@gmail.com>
2023-05-30 19:00:18 -0400

    style updates privacy policy

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -173,18 +173,18 @@ const PrivacyPolicy = () => {
 				</ul>
 				<h2 className="text-2xl my-5 font-bold">Security of Your Personal Data</h2>
 				<p className="my-3">The security of Your Personal Data is important to Us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While We strive to use commercially acceptable means to protect Your Personal Data, We cannot guarantee its absolute security.</p>
-				<h1>Children's Privacy</h1>
+				<h2 className="text-2xl my-5 font-bold">Children's Privacy</h2>
 				<p className="my-3">Our Service does not address anyone under the age of 13. We do not knowingly collect personally identifiable information from anyone under the age of 13. If You are a parent or guardian and You are aware that Your child has provided Us with Personal Data, please contact Us. If We become aware that We have collected Personal Data from anyone under the age of 13 without verification of parental consent, We take steps to remove that information from Our servers.</p>
 				<p className="my-3">If We need to rely on consent as a legal basis for processing Your information and Your country requires consent from a parent, We may require Your parent's consent before We collect and use that information.</p>
-				<h1>Links to Other Websites</h1>
+				<h2 className="text-2xl my-5 font-bold">Links to Other Websites</h2>
 				<p className="my-3">Our Service may contain links to other websites that are not operated by Us. If You click on a third party link, You will be directed to that third party's site. We strongly advise You to review the Privacy Policy of every site You visit.</p>
 				<p className="my-3">We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.</p>
-				<h1>Changes to this Privacy Policy</h1>
+				<h2 className="text-2xl my-5 font-bold">Changes to this Privacy Policy</h2>
 				<p className="my-3">We may update Our Privacy Policy from time to time. We will notify You of any changes by posting the new Privacy Policy on this page.</p>
 				<p className="my-3">We will let You know via email and/or a prominent notice on Our Service, prior to the change becoming effective and update the "Last updated" date at the top of this Privacy Policy.</p>
 				<p className="my-3">You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.</p>
-				<h1>Contact Us</h1>
-				<p className="my-3">If you have any questions about this Privacy Policy, You can contact us by visiting this page on our website: <a href="https://mnmk9-bookings.vercel.app/contact-us" rel="external nofollow noopener" target="_blank">https://mnmk9-bookings.vercel.app/contact-us</a></p>
+				<h2 className="text-2xl my-5 font-bold">Contact Us</h2>
+				<p className="my-3">If you have any questions about this Privacy Policy, You can contact us by visiting this page on our website: <a className="underline" href="https://mnmk9-bookings.vercel.app/contact-us" rel="external nofollow noopener" target="_blank">https://mnmk9-bookings.vercel.app/contact-us</a></p>
 			</section>
 		</div>
 	)
