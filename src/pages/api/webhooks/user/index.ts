51d26e50a05cf3b949de636d7043c92de2d06c09
Matt Vinall <matt.vinall7@gmail.com>
2023-06-04 13:21:21 -0400

    comment to redeploy

--- src/pages/api/webhooks/user/index.ts
+++ src/pages/api/webhooks/user/index.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import { Webhook, WebhookRequiredHeaders } from "svix";
-import type { WebhookEvent } from "@clerk/clerk-sdk-node" 
+import type { WebhookEvent } from "@clerk/clerk-sdk-node"
 import { IncomingHttpHeaders } from "http";
 
 const prisma = new PrismaClient();
+
+// webhook secret
 const webhookSecret = process.env.WEBHOOK_SECRET || "";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -38,7 +40,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const eventType = evt.type as EventType;
   const { id, image_url, first_name, last_name, email_addresses } = evt.data;
   const email = email_addresses?.length > 0 && email_addresses?.map((email: any) => email.email_address)[0];
-  
+
   if (eventType === "user.created") {
     await prisma.user.create({
       data: {
@@ -49,7 +51,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       }
     });
   }
-  
+
   if (eventType === "user.updated") {
     await prisma.user.update({
       where: {
@@ -61,7 +63,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         email: email as string,
       }
     });
-  } 
+  }
 
   res.status(200).json({ message: "Webhook processed successfully" });
 }
