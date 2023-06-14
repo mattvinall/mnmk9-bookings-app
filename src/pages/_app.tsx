a5db8da4e4dd4ab6240dc2ac6e527963eddf8e6d
mattv993 <matt.vinall7@gmail.com>
2023-06-14 16:14:38 -0400

    layout update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,49 @@
 import { type AppType } from "next/app";
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/nextjs'
 import MainLayout from "../components/global/Layout"
 import '../styles/styles.css';
 
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
+import { useRouter } from "next/router";
+
+const publicPages = ["/sign-in/[[...index]]", "/sign-up/[[...index]]"];
+
+const SignedOutRedirect = () => {
+  const router = useRouter();
+
+  router.push("/sign-in");
+
+  return null;
+};
 
 const MyApp: AppType = ({
   Component,
   pageProps: { ...pageProps },
 }) => {
+
+  const router = useRouter();
+
   return (
-    <ClerkProvider>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
+    <ClerkProvider {...pageProps}>
+      {publicPages.includes(router.pathname) ? (
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      ) : (
+        <>
+          <SignedIn>
+            <MainLayout>
+              <Component {...pageProps} />
+            </MainLayout>
+          </SignedIn>
+
+          <SignedOut>
+            <SignedOutRedirect />
+          </SignedOut>
+        </>
+      )}
     </ClerkProvider>
   );
 };
