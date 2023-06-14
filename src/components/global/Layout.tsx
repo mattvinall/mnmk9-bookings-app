32cd1740cfdea5de5cf3e9b6fadd210368d24676
mattv993 <matt.vinall7@gmail.com>
2023-06-13 22:01:37 -0400

    remove script to see if build still fails in prod

--- src/components/global/Layout.tsx
+++ src/components/global/Layout.tsx
@@ -14,7 +14,7 @@ const MainLayout = ({ children }: ILayoutProps) => {
                 <title>MNMK-9 Bookings App</title>
                 <meta name="description" content="MNMK-9 Booking App" />
                 <link rel="icon" href="/favicon.ico" />
-                <script src="https://clerk.mnmk9-bookings.app/npm/@clerk/clerk-js@4/dist/clerk.browser.js"></script>
+                {/* <script src="https://clerk.mnmk9-bookings.app/npm/@clerk/clerk-js@4/dist/clerk.browser.js"></script> */}
             </Head>
             <Navbar />
             <main className="min-h-[85vh] flex justify-center bg-gradient-to-b from-[#112C4F] to-[#15162c]">
