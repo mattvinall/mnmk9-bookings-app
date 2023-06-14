f2601dd21fe2a405344d77aa40a57277a6efb8ea
mattv993 <matt.vinall7@gmail.com>
2023-06-14 00:03:13 -0400

    removed isPublic logic from middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,29 +3,29 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 // Set the paths that don't require the user to be signed in
-const publicPaths = ['/sign-in', '/sign-up', 'contact-us']
+// const publicPaths = ['/sign-in', '/sign-up', 'contact-us']
 
-const isPublic = (path: string) => {
-  return publicPaths.find(x =>
-    path.match(new RegExp(`^${x}$`.replace('*$', '($|/)')))
-  )
-}
+// const isPublic = (path: string) => {
+//   return publicPaths.find(x =>
+//     path.match(new RegExp(`^${x}$`.replace('*$', '($|/)')))
+//   )
+// }
 
 export default withClerkMiddleware((request: NextRequest) => {
-  if (isPublic(request.nextUrl.pathname)) {
-    return NextResponse.next()
-  }
+  // if (isPublic(request.nextUrl.pathname)) {
+  //   return NextResponse.next()
+  // }
   // if the user is not signed in redirect them to the sign in page.
   const { userId } = getAuth(request);
   console.log("user id from middleware", userId);
 
   if (!userId) {
     // redirect the users to /pages/sign-in/[[...index]].ts
-
     const signInUrl = new URL('/sign-in', request.url)
     signInUrl.searchParams.set('redirect_url', request.url)
     return NextResponse.redirect(signInUrl)
   }
+  
   return NextResponse.next()
 })
 
