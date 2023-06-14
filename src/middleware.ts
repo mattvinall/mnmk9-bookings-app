85cecfcddbb5600fa0469bb24e3dd8eafa73d573
mattv993 <matt.vinall7@gmail.com>
2023-06-14 12:17:01 -0400

    new middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,13 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/"]
+  publicRoutes: ["/"],
+  afterAuth(auth, req, evt) {
+    // handle users who aren't authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+  }
 });
 
 export const config = {
