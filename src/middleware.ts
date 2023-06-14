502b549fb3038a14afaf718b2c11af626588105c
mattv993 <matt.vinall7@gmail.com>
2023-06-13 23:55:25 -0400

    middleware update

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 // Set the paths that don't require the user to be signed in
-const publicPaths = ['/', '/sign-in', '/sign-up']
+const publicPaths = ['/sign-in', '/sign-up', 'contact-us']
 
 const isPublic = (path: string) => {
   return publicPaths.find(x =>
@@ -16,7 +16,8 @@ export default withClerkMiddleware((request: NextRequest) => {
     return NextResponse.next()
   }
   // if the user is not signed in redirect them to the sign in page.
-  const { userId } = getAuth(request)
+  const { userId } = getAuth(request);
+  console.log("user id from middleware", userId);
 
   if (!userId) {
     // redirect the users to /pages/sign-in/[[...index]].ts
