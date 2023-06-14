bd4ace3cfe50e98a5ee2b1570dcc10814547ab36
mattv993 <matt.vinall7@gmail.com>
2023-06-13 21:51:53 -0400

    added script as fallback - hope this works :D

--- src/components/global/Layout.tsx
+++ src/components/global/Layout.tsx
@@ -1,26 +1,28 @@
 import React, { ReactNode } from 'react'
+import Script from "next/script";
 import Head from "next/head";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 interface ILayoutProps {
-	children: NonNullable<ReactNode>;
+    children: NonNullable<ReactNode>;
 }
 
 const MainLayout = ({ children }: ILayoutProps) => {
-	return (
-		<>
-			<Head>
-				<title>MNMK-9 Bookings App</title>
-				<meta name="description" content="MNMK-9 Booking App" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<Navbar />
-			<main className="min-h-[85vh] flex justify-center bg-gradient-to-b from-[#112C4F] to-[#15162c]">
-				{children}
-			</main>
-			<Footer />
-		</>
-	)
+    return (
+        <>
+            <Head>
+                <title>MNMK-9 Bookings App</title>
+                <meta name="description" content="MNMK-9 Booking App" />
+                <link rel="icon" href="/favicon.ico" />
+                <Script src="https://clerk.mnmk9-bookings.app/npm/@clerk/clerk-js@4/dist/clerk.browser.js" strategy="beforeInteractive" />
+            </Head>
+            <Navbar />
+            <main className="min-h-[85vh] flex justify-center bg-gradient-to-b from-[#112C4F] to-[#15162c]">
+                {children}
+            </main>
+            <Footer />
+        </>
+    )
 }
 
 export default MainLayout;
