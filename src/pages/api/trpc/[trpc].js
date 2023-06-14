efcf8c9de6622ce64cab61aa81404ca607ebbc1f
mattv993 <matt.vinall7@gmail.com>
2023-06-14 16:25:24 -0400

    test

--- src/pages/api/trpc/[trpc].js
+++ src/pages/api/trpc/[trpc].js
@@ -3,15 +3,16 @@ import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { env } from "../../../env/server.mjs";
 import { createContext } from "../../../server/trpc/context";
 import { appRouter } from "../../../server/trpc/router/_app";
+import { withAuth } from "@clerk/nextjs/api";
 
 // export API handler
-export default createNextApiHandler({
+export default withAuth(createNextApiHandler({
   router: appRouter,
   createContext,
   onError:
     env.NODE_ENV === "development"
       ? ({ path, error }) => {
-          console.error(`❌ tRPC failed on ${path}: ${error}`);
-        }
+        console.error(`❌ tRPC failed on ${path}: ${error}`);
+      }
       : undefined,
-});
+}));
