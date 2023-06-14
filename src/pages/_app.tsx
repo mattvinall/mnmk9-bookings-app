7b63124e459ba7b118a7b26889fb5ff5b3c86c8c
mattv993 <matt.vinall7@gmail.com>
2023-06-14 15:44:18 -0400

    revert back to how _app was

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,5 @@
 import { type AppType } from "next/app";
-import {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  RedirectToSignIn,
-} from "@clerk/nextjs";
-import { AppProps } from "next/app";
-import { useRouter } from "next/router";
+import { ClerkProvider } from '@clerk/nextjs'
 import MainLayout from "../components/global/Layout"
 import '../styles/styles.css';
 
@@ -14,33 +7,15 @@ import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
 
-const publicPages: Array<string> = [];
-
-const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }: AppProps) => {
-  // Get the pathname
-  const { pathname } = useRouter();
-
-  // Check if the current route matches a public page
-  const isPublicPage = publicPages.includes(pathname);
-
+const MyApp: AppType = ({
+  Component,
+  pageProps: { ...pageProps },
+}) => {
   return (
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}>
-      {isPublicPage ? (
-        <MainLayout>
-          <Component {...pageProps} />
-        </MainLayout>
-      ) : (
-        <>
-          <SignedIn>
-            <MainLayout>
-              <Component {...pageProps} />
-            </MainLayout>
-          </SignedIn>
-          <SignedOut>
-            <RedirectToSignIn redirectUrl="/" afterSignInUrl="/" afterSignUpUrl="/" />
-          </SignedOut>
-        </>
-      )}
+    <ClerkProvider>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
     </ClerkProvider>
   );
 };
