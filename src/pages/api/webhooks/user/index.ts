45d5ed0af2b32bddeb03a335547460f6e42ed3bd
mattv993 <matt.vinall7@gmail.com>
2023-06-15 09:38:54 -0400

    cleanup code

--- src/pages/api/webhooks/user/index.ts
+++ src/pages/api/webhooks/user/index.ts
@@ -1,75 +1,3 @@
-// import { Prisma, PrismaClient } from "@prisma/client";
-// import { NextApiRequest, NextApiResponse } from "next";
-// import { Webhook, WebhookRequiredHeaders } from "svix";
-// import type { WebhookEvent } from "@clerk/clerk-sdk-node"
-// import { IncomingHttpHeaders } from "http";
-
-// const prisma = new PrismaClient();
-
-// // webhook secret
-// const webhookSecret = process.env.WEBHOOK_SECRET || "";
-
-// export default async function handler(req: NextApiRequestWithSvixRequiredHeaders, res: NextApiResponse) {
-//   const payload = JSON.stringify(req.body)
-//   console.log("payload", payload);
-
-//   const headers = req.headers;
-
-//   const wh = new Webhook(webhookSecret);
-
-//   let evt: Event | null = null;
-//   try {
-//     evt = wh.verify(payload, headers) as Event;
-//     console.log("event webhook", evt);
-//   } catch (err) {
-//     console.error((err as Error).message);
-//     res.status(400).json({});
-//     return;
-//   }
-
-//   const eventType = evt.type as EventType;
-//   console.log("event type", eventType);
-  
-  
-//   if (eventType === "user.created") {
-//     const { id, image_url, first_name, last_name, email_addresses } = evt.data;
-//     const email = email_addresses?.length > 0 && email_addresses?.map((email: any) => email.email_address)[0];
-//     console.log("email", email);
-//     await prisma.user.create({
-//       data: {
-//         id: id as string,
-//         image: image_url as string,
-//         name: `${first_name} ${last_name}`,
-//         email: email as string,
-//       }
-//     });
-//     console.log(`User ${id} was ${eventType}`);
-//     res.status(201).json({ message: "user was created successfully" });
-//   } else if (eventType === "user.updated") {
-//     const { id, image_url, first_name, last_name, email_addresses } = evt.data;
-//     const email = email_addresses?.length > 0 && email_addresses?.map((email: any) => email.email_address)[0];
-//     await prisma.user.update({
-//       where: {
-//         id: id as string,
-//       },
-//       data: {
-//         image: image_url as string,
-//         name: `${first_name} ${last_name}`,
-//         email: email as string,
-//       }
-//     });
-//     console.log(`User ${id} was ${eventType}`);
-//     res.status(201).json({ message: "user was updated successfully" });
-//   } else { 
-//     console.log("event type not created or updated", eventType);
-//   }
-// }
-
-
-
-
-
-
 import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import { Webhook, WebhookRequiredHeaders } from "svix";
@@ -78,7 +6,6 @@ import { IncomingHttpHeaders } from "http";
 
 const prisma = new PrismaClient();
 
-// webhook secret
 const webhookSecret = process.env.WEBHOOK_SECRET as string || "";
 
 export default async function handler(req: NextApiRequestWithSvixRequiredHeaders, res: NextApiResponse) {
@@ -91,13 +18,6 @@ export default async function handler(req: NextApiRequestWithSvixRequiredHeaders
   console.log("payload", payload);
 
   const headers = req.headers;
-
-  // const headersList = req.headers;
-  // const heads = {
-  //   "svix-id": headersList["svix-id"],
-  //   "svix-timestamp": headersList["svix-timestamp"],
-  //   "svix-signature": headersList["svix-signature"],
-  // };
 
   const wh = new Webhook(webhookSecret);
 
