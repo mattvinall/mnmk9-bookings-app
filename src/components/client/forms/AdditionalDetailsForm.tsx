3024069680882ba5349660b70bf1c8b06ddec3cd
mattv993 <matt.vinall7@gmail.com>
2023-06-24 10:22:39 -0400

    removed all react hook form and other imports, moving to separate components

--- src/components/client/forms/AdditionalDetailsForm.tsx
+++ src/components/client/forms/AdditionalDetailsForm.tsx
@@ -1,22 +1,28 @@
 "use client";
 
 import { useEffect, useState, useCallback, Dispatch, SetStateAction } from 'react';
-import { useForm, SubmitHandler } from "react-hook-form";
-import { zodResolver } from '@hookform/resolvers/zod';
-import { trpc } from "../../../utils/trpc";
-import Swal from "sweetalert2";
 import { GoogleReCaptcha, useGoogleReCaptcha } from "react-google-recaptcha-v3";
 import { useAuth } from "@clerk/nextjs";
 import { useRouter } from "next/router";
+import { Pet, User } from '@prisma/client';
+import VaccineForm from './VaccineForm';
+import VetForm from './VetForm';
 
-type Props = {
-    setShowAdditionalDetailsForm: Dispatch<SetStateAction<boolean>>
-    secret: string;
+
+interface UserDetail extends User {
+    pets: Pet[]
 }
 
-const AdditionalDetailsForm = ({ setShowAdditionalDetailsForm, secret }: Props) => {
+type Props = {
+    setShowAdditionalDetailsForm: Dispatch<SetStateAction<boolean>>,
+    secret: string;
+    userDetail: UserDetail
+}
+
+const AdditionalDetailsForm = ({ userDetail, setShowAdditionalDetailsForm, secret }: Props) => {
     const [token, setToken] = useState<string>("");
     const [score, setScore] = useState<number | null>(null);
+    const [isUpdatingVetInfo, setIsUpdatingVetInfo] = useState(true);
 
     const { userId } = useAuth();
 
@@ -40,9 +46,100 @@ const AdditionalDetailsForm = ({ setShowAdditionalDetailsForm, secret }: Props) 
     }, [handleReCaptchaVerify]);
 
     const router = useRouter();
+
+    const handleToggle = () => {
+        setIsUpdatingVetInfo(!isUpdatingVetInfo);
+    };
+
     return (
-        <></>
-    )
+        <div>
+            <button className="rounded-full text-xs font-bold text-white bg-red-500 py-1 px-2" onClick={handleToggle}>
+                {isUpdatingVetInfo ? 'Add Vaccination Documents' : 'Update Vet Information'}
+            </button>
+            {isUpdatingVetInfo ? (
+                <VetForm userDetail={userDetail} />
+                // <form onSubmit={handleSubmit(onSubmit)}>
+                //     <label>
+                //         Pet:
+                //         <select
+                //             value={selectedPet}
+                //             {...register('petName')}
+                //         >
+                //             {userDetail?.pets?.map((pet) => (
+                //                 <option key={pet} value={pet}>
+                //                     {pet}
+                //                 </option>
+                //             ))}
+                //         </select>
+                //     </label>
+                //     <label>
+                //         Vet Name:
+                //         <input
+                //             type="text"
+                //             name="name"
+                //             {...register('name')}
+                //         />
+                //         {errors.vetName && <span>{errors.name.message}</span>}
+                //     </label>
+                //     <label>
+                //         Vet Phone Number:
+                //         <input
+                //             type="text"
+                //             name="vetPhoneNumber"
+                //             {...register('vetPhoneNumber')}
+                //         />
+                //         {errors.vetPhoneNumber && <span>{errors.vetPhoneNumber.message}</span>}
+                //     </label>
+                //     <label>
+                //         Vet Email:
+                //         <input
+                //             type="text"
+                //             name="vetEmail"
+                //             {...register('vetEmail')}
+                //         />
+                //         {errors.vetEmail && <span>{errors.vetEmail.message}</span>}
+                //     </label>
+                //     <label>
+                //         Vet Address:
+                //         <input
+                //             type="text"
+                //             name="vetAddress"
+                //             {...register('vetAddress')}
+                //         />
+                //         {errors.vetAddress && <span>{errors.vetAddress.message}</span>}
+                //     </label>
+                //     <button type="submit">
+                //         Submit
+                //     </button>
+                // </form>
+            ) : (
+                <VaccineForm userDetail={userDetail} />
+                // <form onSubmit={handleSubmit(onSubmit)}>
+                //     <label>
+                //         Vaccine Name:
+                //         <input
+                //             type="text"
+                //             name="vaccineName"
+                //             {...register('vaccineName')}
+                //         />
+                //         {errors.vaccineName && <span>{errors.vaccineName.message}</span>}
+                //     </label>
+                //     <label>
+                //         Valid Until:
+                //         <input
+                //             type="text"
+                //             name="vaccineValidUntil"
+                //             {...register('vaccineValidUntil')}
+                //         />
+                //         {errors.vaccineValidUntil && <span>{errors.vaccineValidUntil.message}</span>}
+                //     </label>
+                //     <button type="submit">
+                //         Submit
+                //     </button>
+                // </form>
+            )}
+        </div>
+    );
 }
 
-export default AdditionalDetailsForm
+export default AdditionalDetailsForm;
