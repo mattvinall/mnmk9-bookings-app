b637aebe792d46e4366c3dca0d38e5f86ea63e48
mattv993 <matt.vinall7@gmail.com>
2023-06-27 12:39:41 -0400

    formatDate util function

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,8 @@
+export const formatDate = (date: string) => {
+    return new Date(date).toLocaleDateString("en-US", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    });
+}
