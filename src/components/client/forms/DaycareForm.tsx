0a0bbbcc1c00eb1a938060e1e013aaae2d848770
mattv993 <matt.vinall7@gmail.com>
2023-06-28 12:09:54 -0400

    extract trpc logic from client components

--- src/components/client/forms/DaycareForm.tsx
+++ src/components/client/forms/DaycareForm.tsx
@@ -1,6 +1,6 @@
 "use client";
+
 import { useEffect, useCallback } from "react";
-import { trpc } from "../../../utils/trpc";
 import {
 	useGoogleReCaptcha,
 	GoogleReCaptcha,
@@ -8,10 +8,12 @@ import {
 import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
 import type { FormTypeProps } from "../../../types/form-types";
 import type { Pet } from "@prisma/client";
+import { getUserById } from "../../../api/users";
 
 const DaycareForm = ({ register, setToken, setValue, handleSubmit, onSubmit, handleChange, petData, isSubmitting }: FormTypeProps): ReactJSXElement => {
 	const id = petData && petData?.map((pet: Pet) => pet.ownerId)[0] as string;
-	const { data: userData } = trpc.user.byId.useQuery({ id });
+
+	const { data: userData } = getUserById(id);
 
 	const { executeRecaptcha } = useGoogleReCaptcha()
 	// Create an event handler so you can call the verification on button click event or form submit
