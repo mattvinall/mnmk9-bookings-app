2b217fa4f8498134d430eb9e60b5836f0b22ceb7
mattv993 <matt.vinall7@gmail.com>
2023-07-04 12:39:31 -0400

    update to profile page - added more dropdown to route to add vaccination documents / vet info

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -7,10 +7,10 @@ import { useRouter } from "next/router";
 import { trpc } from "../../utils/trpc";
 import UserDetailForm from "../../components/client/forms/UserDetailForm";
 import AddPetForm from "../../components/client/forms/AddPetForm";
-import AdditionalDetailsForm from "../../components/client/forms/AdditionalDetailsForm";
 import Swal from "sweetalert2";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
-import { Pet } from "../../types/router";
+// import { Pet } from "../../types/router";
+import { Pet } from "@prisma/client";
 import useSetWaiverDocument from "../../hooks/useSetWaiverDocument";
 import React from "react";
 import { AddWaiverForm } from "../../components/client/forms/AddWaiverForm";
@@ -22,7 +22,6 @@ const UserDetail = () => {
 	const [showUserForm, setShowUserForm] = useState<boolean>(false);
 	const [showPetForm, setShowPetForm] = useState<boolean>(false);
 	const [showPets, setShowPets] = useState<boolean>(false);
-	const [showAdditionalDetailsForm, setShowAdditionalDetailsForm] = useState<boolean>(false);
 	const [showAddWaiverForm, setshowAddWaiverForm] = useState<boolean>(false)
 	const [key, setKey] = useState<string>("")
 	const [secret, setSecret] = useState<string>("");
@@ -50,41 +49,41 @@ const UserDetail = () => {
 		setShowUserForm(true);
 		setShowPetForm(false);
 		setShowPets(false);
-		setShowAdditionalDetailsForm(false);
 		setshowAddWaiverForm(false);
+		setMenuShow(false);
 	}
 
 	const handleShowPetForm = () => {
 		setShowPetForm(true);
 		setShowUserForm(false);
 		setShowPets(false);
-		setShowAdditionalDetailsForm(false);
 		setshowAddWaiverForm(false);
+		setMenuShow(false);
 	}
 
 	const handleShowPets = () => {
 		setShowPets(true);
 		setShowPetForm(false);
 		setShowUserForm(false);
-		setShowAdditionalDetailsForm(false);
 		setshowAddWaiverForm(false);
-	}
-
-	const handleShowVetForm = () => {
-		setShowAdditionalDetailsForm(true);
-		setShowPets(false);
-		setShowPetForm(false);
-		setShowUserForm(false);
-		setshowAddWaiverForm(false);
+		setMenuShow(false);
 	}
 
 	const handleShowAddWaiverForm = () => {
 		setshowAddWaiverForm(true);
-		setShowAdditionalDetailsForm(false);
 		setShowPets(false);
 		setShowPetForm(false);
 		setShowUserForm(false);
+		setMenuShow(false);
 	}
+
+	const handleAddButtonClick = () => {
+		setMenuShow(!menuShow);
+		setshowAddWaiverForm(false);
+		setShowPets(false);
+		setShowPetForm(false);
+		setShowUserForm(false);
+	};
 
 	useEffect(() => {
 		setShowPetForm(true);
@@ -163,8 +162,6 @@ const UserDetail = () => {
 		petName && setPetName(petName);
 	}
 
-	const handleAddButtonClick = () => setMenuShow(!menuShow);
-
 	const handleDeletePet = async (id: string, name: string) => {
 		try {
 			Swal.fire({
@@ -199,7 +196,13 @@ const UserDetail = () => {
 		</div>
 	);
 
-	if (error) router.push("/");
+	if (error) {
+		return (
+			<div className="container text-center">
+				<h1 className="text-1xl font-extrabold mt-[15%] tracking-tight text-white sm:text-[2rem]">Sorry Something Went Wrong....</h1>
+			</div>
+		)
+	}
 
 	return (
 		<div className="container flex flex-col items-center justify-start gap-12 px-4 py-16">
@@ -222,14 +225,27 @@ const UserDetail = () => {
 					<li className="mr-2">
 						<button onClick={handleShowUserForm} className={`${showUserForm === true ? "text-gray-100 !border-gray-100 border-b-2" : ""} inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-100 hover:border-gray-100`}>Edit Profile</button>
 					</li>
-					<li>
-						<button onClick={handleShowVetForm} className={`${showAdditionalDetailsForm === true ? "text-gray-100 !border-gray-100 border-b-2" : ""} inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-100 hover:border-gray-100`}>Additional Information</button>
-					</li>
 					<li className="mr-2">
 						<a href="https://mnmk9-bookings.s3.ca-central-1.amazonaws.com/documents/waiver/mnmk9-waiver.docx" className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-100 hover:border-gray-100" download="MNMK-9 Waiver">Download Waiver</a>
 					</li>
 					<li className="mr-2">
 						<button onClick={handleShowAddWaiverForm} className={`${showAddWaiverForm === true ? "text-gray-100 !border-gray-100 border-b-2" : ""} inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-100 hover:border-gray-100`}>Upload Waiver</button>
+					</li>
+					<li className="relative">
+						<button onClick={handleAddButtonClick} className={`${menuShow === true ? "text-gray-100 !border-gray-100 border-b-2" : ""} inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-100 hover:border-gray-100`} id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover">
+							More +
+						</button>
+						{/* Dropdown Menu */}
+						<div id="dropdownHover" className={`${!menuShow ? `hidden` : "transition-block block absolute z-10 top-[4rem] right-0 bg-white divide-y divide-gray-100 rounded-lg shadow w-auto dark:bg-gray-700"}`}>
+							<ul className="py-2 text-sm text-gray-700 dark:text-gray-200 w-32" aria-labelledby="dropdownHoverButton">
+								<li>
+									<a href="/profile/vaccinations" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Vaccinations</a>
+								</li>
+								<li>
+									<a href="/profile/vet-info" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Vet Info</a>
+								</li>
+							</ul>
+						</div>
 					</li>
 				</ul>
 			</div>
@@ -250,15 +266,8 @@ const UserDetail = () => {
 					handleSubmit={handleSubmit}
 					fileName={fileName}
 					handleWaiverDocumentFileChange={handleWaiverDocumentFileChange}
-				/>}
-			{userDetail && showAdditionalDetailsForm && (
-				<GoogleReCaptchaProvider reCaptchaKey={key}>
-					<AdditionalDetailsForm
-						userDetail={userDetail}
-						setShowAdditionalDetailsForm={setShowAdditionalDetailsForm}
-						secret={secret} />
-				</GoogleReCaptchaProvider>
-			)}
+				/>
+			}
 
 			<div className="grid grid-cols-1 gap-4 lg:grid-cols-3 md:grid-cols-2 md:gap-8 mt-10">
 				{showPets && userDetail?.pets?.map((pet: Pet, index: number) => {
@@ -270,7 +279,7 @@ const UserDetail = () => {
 									<div className="p-6">
 										<h2 className="text-gray-900 text-xl font-medium mb-2">{pet.name}</h2>
 										<p className="text-gray-700 text-base mb-4">{pet.breed}</p>
-										<p className="text-gray-600 font-bold text-xs">Vaccinated: {pet?.vaccinated === false ? "❌" : "✅"}</p>
+										<p className="text-gray-600 font-bold text-xs">Neutered: {pet?.ovariohysterectomy === false ? "❌" : "✅"}</p>
 										<div className="flex bg-white">
 											<Link href={`/pet/${pet.id}`} className="mt-6 flex flex-col items-center justify-center w-12 h-12 mr-2 text-gray-900 transition-colors duration-150 bg-teal-600 rounded-full focus:shadow-outline hover:bg-teal-500">
 												<svg className="w-5 h-5 fill-current" viewBox="0 0 20 20"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path></svg>
@@ -280,22 +289,6 @@ const UserDetail = () => {
 													<path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"></path>
 												</svg>
 											</button>
-											<button onClick={handleAddButtonClick} className="mt-6 ml-4 flex flex-col items-center justify-center w-12 h-12 mr-2 text-gray-900 transition-colors duration-150 bg-teal-600 rounded-full focus:shadow-outline hover:bg-teal-500" id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover">
-												<svg className="w-5 h-5" fill="#fff" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-													<path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6"></path>
-												</svg>
-											</button>
-											{/* Dropdown Menu */}
-											<div id="dropdownHover" className={`${!menuShow ? `hidden` : "transition-block block z-10 relative top-20 right-12 bg-white divide-y divide-gray-100 rounded-lg shadow w-auto dark:bg-gray-700"}`}>
-												<ul className="py-2 text-sm text-gray-700 dark:text-gray-200 w-32" aria-labelledby="dropdownHoverButton">
-													<li>
-														<a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Vaccinations</a>
-													</li>
-													<li>
-														<a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Vet Info</a>
-													</li>
-												</ul>
-											</div>
 										</div>
 									</div>
 								</div>
