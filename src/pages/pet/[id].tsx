a48d050464b49f2f059c75eba68611a106d77ad8
mattv993 <matt.vinall7@gmail.com>
2023-07-06 10:26:55 -0400

    update to pet detail page UI; card on left, form to edit on the right; stacked on mobile

--- src/pages/pet/[id].tsx
+++ src/pages/pet/[id].tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import { trpc } from "../../utils/trpc";
@@ -8,101 +8,79 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from "zod";
 import Swal from "sweetalert2";
-
-type EditPetNotes = {
-	notes: string
-}
-const schema = z.object({
-	notes: z.string()
-})
+import { Pet } from "@prisma/client";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+import EditPetForm from "../../components/client/forms/EditPetForm";
 
 const PetDetail = () => {
+	const [secret, setSecret] = useState<string>("");
+	const [key, setKey] = useState<string>("");
 
 	const router = useRouter();
 	const id = router.query.id as string;
 	const { data: petDetail, isLoading, error, refetch } = trpc.pet.byId.useQuery({ id });
 
 	// get values of name, id and vaccinated to use for later
-	const name = petDetail?.map(pet => pet.name as string)[0];
-	const petId = petDetail?.map(pet => pet.id as string)[0];
-	const vaccinated = petDetail?.map(pet => pet.vaccinated)[0];
-	const ownerId = petDetail?.map(pet => pet.ownerId as string)[0];
+	const name = petDetail?.map((pet: Pet) => pet.name as string)[0];
+	const petId = petDetail?.map((pet: Pet) => pet.id as string)[0];
+	const ownerId = petDetail?.map((pet: Pet) => pet.ownerId as string)[0];
 
-	const {
-		uploadedProfileImageUrl,
-		imageFileNamePreview,
-		handleProfileImageFileChange,
-		handleUploadProfileImage
-	} = useSetProfileImage(name);
+	// const {
+	// 	uploadedProfileImageUrl,
+	// 	imageFileNamePreview,
+	// 	handleProfileImageFileChange,
+	// 	handleUploadProfileImage
+	// } = useSetProfileImage(name as string);
 
-	const {
-		uploadedVaccinationDocumentUrl,
-		handleVaccinationDocumentFileChange,
-	} = useSetVaccinationDocument(name);
+	// const {
+	// 	uploadedVaccinationDocumentUrl,
+	// 	handleVaccinationDocumentFileChange,
+	// } = useSetVaccinationDocument(name as string);
 
 	// mutations to add profile image, vaccination document, delete document, and update vaccinated status
-	const uploadPetProfileImage = trpc.pet.addPetProfilePicture.useMutation({
-		onSuccess: () => refetch()
-	});
-	const uploadVaccinationDocument = trpc.documents.addVaccinationDocument.useMutation({
-		onSuccess: () => refetch()
-	});
-	const deleteVaccinationDocument = trpc.documents.deleteVaccinationDocument.useMutation({
-		onSuccess: () => refetch()
-	});
-	const updateVaccinatedStaus = trpc.pet.updateVaccinatedStatus.useMutation({
-		onSuccess: () => refetch()
-	});
 
-	const updatePetNotes = trpc.pet.editPetNotes.useMutation({
-		onSuccess: () => refetch()
-	});
+	// const uploadPetProfileImage = trpc.pet.addPetProfilePicture.useMutation({
+	// 	onSuccess: () => refetch()
+	// });
+	// const uploadVaccinationDocument = trpc.vaccine.create.useMutation({
+	// 	onSuccess: () => refetch()
+	// });
+	// const deleteVaccinationDocument = trpc.vaccine.delete.useMutation({
+	// 	onSuccess: () => refetch()
+	// });
 
 	// once profile image has been updated, add to Pet DB tale by passing in the pet id and the image url from S3
-	useEffect(() => {
-		if (uploadedProfileImageUrl) {
-			uploadPetProfileImage.mutate({ id, profileImage: uploadedProfileImageUrl as string });
-		}
-	}, [uploadedProfileImageUrl])
+
+	// useEffect(() => {
+	// 	if (uploadedProfileImageUrl) {
+	// 		uploadPetProfileImage.mutate({ id, profileImage: uploadedProfileImageUrl as string });
+	// 	}
+	// }, [uploadedProfileImageUrl])
 
 	// if there is a vaccinated document url from S3, add to the Documents table by passing in the pet id and the file name
+
+	// useEffect(() => {
+	// 	if (uploadedVaccinationDocumentUrl && petId) {
+	// 		uploadVaccinationDocument.mutate({
+	// 			petId,
+	// 			fileName: uploadedVaccinationDocumentUrl.split(".")[0] as string,
+	// 			uploadedS3Url: uploadedVaccinationDocumentUrl,
+	// 			validTo: new Date("2024-01-01"),
+	// 			name: "Lepto Vaccine"
+	// 		});
+	// 	}
+	// }, [uploadedVaccinationDocumentUrl]);
+
 	useEffect(() => {
-		if (uploadedVaccinationDocumentUrl) {
-			uploadVaccinationDocument.mutate({ petId: id, fileName: uploadedVaccinationDocumentUrl as string })
-		}
+		const key = process.env.NEXT_PUBLIC_RECAPTCHA_SITEKEY;
+		const secret = process.env.NEXT_PUBLIC_RECAPTCHA_SECRET;
 
-		// if pet is not vaccinated and uploaded a document, update status to true/yes
-		if (petId && vaccinated === false) {
-			updateVaccinatedStaus.mutate({ id: petId, vaccinated })
-		}
-	}, [uploadedVaccinationDocumentUrl]);
+		if (!key) return;
+		if (!secret) return;
 
-	const { register, handleSubmit, reset, formState: { isSubmitting } } = useForm<EditPetNotes>({
-		resolver: zodResolver(schema)
-	});
-
-	const onSubmit: SubmitHandler<EditPetNotes> = async (formData: any) => {
-		try {
-			formData.id = id as string
-
-			// success message 
-			Swal.fire({
-				icon: 'success',
-				title: `✅`,
-				text: `Successfully updated ${name}'s notes on file`,
-			});
-
-			updatePetNotes.mutate({ id, notes: formData.notes })
-			reset()
-
-		} catch (error) {
-			Swal.fire({
-				icon: 'error',
-				title: 'Oops...',
-				text: `Something went wrong! ${error}`,
-			});
-		}
-	}
+		setKey(key);
+		setSecret(secret);
+	}, [key, secret]);
 
 	if (isLoading) return (
 		<div className="container text-center">
@@ -117,84 +95,71 @@ const PetDetail = () => {
 	);
 
 	return (
-		<div className="container flex flex-col items-center justify-start gap-12 px-4 py-16 max-w-8xl">
+		<div className="container flex flex-col gap-12 px-16 py-16 max-w-8xl">
 			<a className="flex justify-start text-left text-white font-bold text-2xl" href={`/profile/${ownerId}`}>Go Back</a>
 			<h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">Pet <span className="text-[rgb(103,163,161)]">Details</span></h1>
-			<div className="m-auto">
+			<div className="flex justify-between items-baseline">
 				{petDetail?.map((pet, idx) => {
 					const defaultImage = `https://mdbootstrap.com/img/new/standard/nature/19${idx + 1}.jpg`
 					return (
 						<div key={pet.id} className="flex justify-center">
-							<div className="rounded-lg shadow-lg bg-white max-w-full w-[300px] md:w-[32rem]">
-								<Image width={550} height={350} className="w-full md:w-full rounded-t-lg h-[350px] object-cover object-center" src={pet.profileImage || uploadedProfileImageUrl || defaultImage} alt={pet.name} />
+							<div className="rounded-lg shadow-lg bg-white max-w-full w-[90%] md:w-[32rem]">
 								<div className="p-6">
-									<h2 className="text-gray-900 text-xl font-medium mb-2">{pet.name}</h2>
-									<p className="text-gray-700 text-base mb-4">{pet.breed}</p>
-									<p className="text-gray-600 font-bold text-xs">Vaccinated: {pet?.vaccinated === false ? "❌" : "✅"}</p>
-									<form className="mt-6" onSubmit={handleSubmit(onSubmit)}>
-										<label htmlFor="notes">Notes</label>
-										<textarea
-											{...register("notes")}
-											id="notes"
-											rows={2}
-											className="block mb-6 py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none border-gray-500 dark:focus:border-gray-900 focus:outline-none focus:ring-0 focus:border-gray-900 peer"
-											defaultValue={pet?.notes ?? ""}
-										/>
-										<button
-											className="mt-[25px] rounded-full bg-gradient-to-l from-[#67A3A1] to-[#112B4E] hover:bg-gradient-to-r from-[#112B4E] to-[#67A3A1] px-16 py-3 font-semibold text-white no-underline transition py-3 px-5 text-sm font-medium text-center rounded-lg bg--700 sm:w-fit focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-											Update
-										</button>
-										<p className="block font-medium py-6 text-md">Upload:</p>
-										<label style={{ cursor: "pointer" }} htmlFor="pet-profile-image" className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-5">
-											Select Profile Image to Upload
-											<input
-												style={{ cursor: "pointer" }}
-												type="file"
-												accept="image/*"
-												id="pet-profile-image"
-												className="hidden"
-												onChange={handleProfileImageFileChange}
-											/>
-										</label>
-										{imageFileNamePreview ? (<button className="bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2" onClick={handleUploadProfileImage}>Upload Selected Profile Image</button>) : null}
-										<label style={{ cursor: "pointer" }} htmlFor="vaccination-documents" className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-											Upload Proof of Vaccinations
-											<input
-												style={{ cursor: "pointer" }}
-												type="file"
-												multiple
-												accept=".pdf, image/*, .png, .jpg, .jpeg"
-												id="vaccination-documents"
-												className="hidden"
-												onChange={handleVaccinationDocumentFileChange}
-											/>
-										</label>
-									</form>
-									{imageFileNamePreview && <p className="font-medium">Image Selected: {imageFileNamePreview}. <br />Click Upload to set this image.</p>}
-									{pet?.documents && pet?.documents.length > 0 && <h2 className="text-gray-900 text-xl font-medium mb-2 mt-6">Documents</h2>}
-									{pet?.documents && pet?.documents.length > 0 && <p className="pb-8 text-sm text-grey-100">*your vaccination document will not upload again if you have the same file name. If you have a revised version, delete the existing one and re-upload the file again.</p>}
-									{pet.documents?.map(doc => {
-										console.log("doc file name", doc.fileName);
-										const fileName = doc.fileName.includes("amazonaws.com") ? doc.fileName.split("/")[5] : doc.fileName;
-										return (
-											<div key={doc.id} style={{ position: "relative" }}>
-												<svg onClick={() => deleteVaccinationDocument.mutate({ id: doc.id })} className="w-6 h-6" fill="#fff" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" style={{ cursor: "pointer", position: "absolute", right: "0px", top: "35%" }}><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>
-												<a className="flex items-center" href={doc.fileName} target="_blank" rel="noreferrer">
-													<svg className="w-[80px]" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-														<path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"></path>
-													</svg>
-													{fileName}
-												</a>
-											</div>
-										)
-									})}
+									<div className="flex justify-betweem items-center border-slate-600 border-b-2 pb-2">
+										<h2 className="text-gray-900 text-3xl font-medium mb-2 w-3/4">{pet.name} - {pet.breed}</h2>
+										<figure className="w-1/4 flex justify-end align-center">
+											<img src={pet.profileImage ? pet.profileImage : defaultImage} alt="pet profile image" className="w-10 h-10 rounded-full border-2 border-gray-300" />
+										</figure>
+									</div>
+									<div className="my-4 border-slate-500 border-b-2">
+										<div className="mb-4">
+											<p className="text-gray-600 font-medium text-lg">Sex: {pet.sex.toLowerCase()}</p>
+										</div>
+										<div className="mb-4">
+											<p className="text-gray-600 font-medium text-lg">Age: {pet.age} years old</p>
+										</div>
+										<div className="mb-4">
+											<p className="text-gray-600 font-medium text-lg">Weight: {pet.weight} lbs</p>
+										</div>
+										<div className="mb-4">
+											<p className="text-gray-600 font-medium text-lg">Temperament: {pet.temperament.toLocaleLowerCase()}</p>
+										</div>
+									</div>
+									<div className="flex flex-col md:flex-row justify-between items-center border-slate-500 border-b-2">
+										<div className="mb-4">
+											<p className="text-gray-600 font-medium text-lg">{pet?.sex === "MALE" ? "Neutered: " : "Spayed: "}  <span className="inline-block ml-2">{pet?.ovariohysterectomy === false ? "❌" : "✅"}</span></p>
+										</div>
+										<div className="mb-4">
+										</div>
+										{/* <p className="text-gray-600 font-medium text-lg">}</span></p> */}
+									</div>
+									<div className="my-4">
+										{pet?.medicalNotes && (
+											<p className="text-gray-600 font-medium text-lg">Medical Notes: {pet.medicalNotes}</p>
+										)}
+									</div>
+									<div className="my-4 border-slate-500 border-b-2 pb-4">
+										{pet?.feedingNotes && (
+											<p className="text-gray-600 font-medium text-lg">Feeding Notes: {pet.feedingNotes}</p>
+										)}
+									</div>
+									{pet?.microchipNumber && (
+										<div className="mt-8 flex justify-center items-end">
+											<p className="min-h-[50px] text-gray-600 font-medium text-sm">Microchip #: {pet.microchipNumber}</p>
+										</div>
+									)}
 								</div>
 							</div>
 						</div>
 					)
 				})}
+				{petId && key && petDetail && (
+					<GoogleReCaptchaProvider reCaptchaKey={key}>
+						<EditPetForm secret={secret} petId={petId} petDetail={petDetail || []} />
+					</GoogleReCaptchaProvider>
+				)}
 			</div>
-		</div>
+		</div >
 	)
 }
 
