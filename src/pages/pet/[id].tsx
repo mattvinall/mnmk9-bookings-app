79cdb87690af046f82560dfa4433c3683d846298
mattv993 <matt.vinall7@gmail.com>
2023-07-06 14:17:03 -0400

    state logic to handle hide/show of content related to clicking of the tabs/navigation

--- src/pages/pet/[id].tsx
+++ src/pages/pet/[id].tsx
@@ -4,17 +4,21 @@ import Image from "next/image";
 import { trpc } from "../../utils/trpc";
 import useSetProfileImage from "../../hooks/useSetProfileImage";
 import useSetVaccinationDocument from "../../hooks/useSetVaccinationDocument";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm, SubmitHandler, set } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from "zod";
 import Swal from "sweetalert2";
 import { Pet } from "@prisma/client";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import EditPetForm from "../../components/client/forms/EditPetForm";
+import { PetDetailCard } from "../../components/client/ui/PetDetailCard";
 
 const PetDetail = () => {
 	const [secret, setSecret] = useState<string>("");
 	const [key, setKey] = useState<string>("");
+	const [showPetDetails, setShowPetDetails] = useState<boolean>(true);
+	const [showVaccinationRecords, setShowVaccinationRecords] = useState<boolean>(false);
+	const [showBookings, setShowBookings] = useState<boolean>(false);
 
 	const router = useRouter();
 	const id = router.query.id as string;
@@ -82,6 +86,25 @@ const PetDetail = () => {
 		setSecret(secret);
 	}, [key, secret]);
 
+	const handleShowPetDetails = () => {
+		setShowPetDetails(true);
+		setShowVaccinationRecords(false);
+		setShowBookings(false);
+	}
+
+	const handleShowVaccinationRecords = () => {
+		setShowPetDetails(false);
+		setShowVaccinationRecords(true);
+		setShowBookings(false);
+	}
+
+	const handleShowBookings = () => {
+		setShowPetDetails(false);
+		setShowVaccinationRecords(false);
+		setShowBookings(true);
+	}
+
+
 	if (isLoading) return (
 		<div className="container text-center">
 			<h1 className="text-1xl font-extrabold mt-[15%] tracking-tight text-white sm:text-[2rem]">Loading....</h1>
@@ -94,70 +117,56 @@ const PetDetail = () => {
 		</div>
 	);
 
+	const defaultImage = `https://mdbootstrap.com/img/new/standard/nature/190.jpg`;
+
 	return (
 		<div className="container flex flex-col gap-12 px-16 py-16 max-w-8xl">
 			<a className="flex justify-start text-left text-white font-bold text-2xl" href={`/profile/${ownerId}`}>Go Back</a>
 			<h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">Pet <span className="text-[rgb(103,163,161)]">Details</span></h1>
+
+			{/* Tabs */}
+			<div className="text-md font-medium text-center text-gray-500 border-b border-gray-500 dark:text-gray-400 dark:border-gray-500">
+				<ul className="w-auto flex flex-wrap">
+					<li className="mr-2">
+						<button onClick={handleShowPetDetails} className={`${showPetDetails === true ? "text-gray-100 !border-gray-100 border-b-2" : ""}  inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-100 hover:border-gray-100`}>Pet Details</button>
+					</li>
+					<li>
+						<button onClick={handleShowVaccinationRecords} className={`${showVaccinationRecords === true ? "text-gray-100 !border-gray-100 border-b-2" : ""} inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-100 hover:border-gray-100`}>Vaccination Records</button>
+					</li>
+					<li className="mr-2">
+						<button onClick={handleShowBookings} className={`${showBookings === true ? "text-gray-100 !border-gray-100 border-b-2" : ""} inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-100 hover:border-gray-100`}>Bookings</button>
+					</li>
+				</ul>
+			</div>
+
+			{/* content  */}
 			<div className="flex justify-between items-baseline">
-				{petDetail?.map((pet, idx) => {
-					const defaultImage = `https://mdbootstrap.com/img/new/standard/nature/19${idx + 1}.jpg`
-					return (
-						<div key={pet.id} className="flex justify-center">
-							<div className="rounded-lg shadow-lg bg-white max-w-full w-[90%] md:w-[32rem]">
-								<div className="p-6">
-									<div className="flex justify-betweem items-center border-slate-600 border-b-2 pb-2">
-										<h2 className="text-gray-900 text-3xl font-medium mb-2 w-3/4">{pet.name} - {pet.breed}</h2>
-										<figure className="w-1/4 flex justify-end align-center">
-											<img src={pet.profileImage ? pet.profileImage : defaultImage} alt="pet profile image" className="w-10 h-10 rounded-full border-2 border-gray-300" />
-										</figure>
-									</div>
-									<div className="my-4 border-slate-500 border-b-2">
-										<div className="mb-4">
-											<p className="text-gray-600 font-medium text-lg">Sex: {pet.sex.toLowerCase()}</p>
-										</div>
-										<div className="mb-4">
-											<p className="text-gray-600 font-medium text-lg">Age: {pet.age} years old</p>
-										</div>
-										<div className="mb-4">
-											<p className="text-gray-600 font-medium text-lg">Weight: {pet.weight} lbs</p>
-										</div>
-										<div className="mb-4">
-											<p className="text-gray-600 font-medium text-lg">Temperament: {pet.temperament.toLocaleLowerCase()}</p>
-										</div>
-									</div>
-									<div className="flex flex-col md:flex-row justify-between items-center border-slate-500 border-b-2">
-										<div className="mb-4">
-											<p className="text-gray-600 font-medium text-lg">{pet?.sex === "MALE" ? "Neutered: " : "Spayed: "}  <span className="inline-block ml-2">{pet?.ovariohysterectomy === false ? "❌" : "✅"}</span></p>
-										</div>
-										<div className="mb-4">
-										</div>
-										{/* <p className="text-gray-600 font-medium text-lg">}</span></p> */}
-									</div>
-									<div className="my-4">
-										{pet?.medicalNotes && (
-											<p className="text-gray-600 font-medium text-lg">Medical Notes: <br /> {pet.medicalNotes}</p>
-										)}
-									</div>
-									<div className="my-4 border-slate-500 border-b-2 pb-4">
-										{pet?.feedingNotes && (
-											<p className="text-gray-600 font-medium text-lg">Feeding Notes: <br /> {pet.feedingNotes}</p>
-										)}
-									</div>
-									{pet?.microchipNumber && (
-										<div className="mt-8 flex justify-center items-end">
-											<p className="min-h-[50px] text-gray-600 font-medium text-sm">Microchip #: {pet.microchipNumber}</p>
-										</div>
-									)}
-								</div>
-							</div>
-						</div>
-					)
-				})}
-				{petId && key && petDetail && (
-					<GoogleReCaptchaProvider reCaptchaKey={key}>
-						<EditPetForm secret={secret} petId={petId} petDetails={petDetail || {}} />
-					</GoogleReCaptchaProvider>
+				{showPetDetails && (
+					<>
+						{/* Pet Detail Card */}
+						{petDetail && petDetail.length > 0 && petDetail.map((pet: Pet) => PetDetailCard(pet, defaultImage))}
+						{/* Edit Pet Form */}
+						{petId && key && petDetail && (
+							<GoogleReCaptchaProvider reCaptchaKey={key}>
+								<EditPetForm secret={secret} petId={petId} petDetails={petDetail || {}} />
+							</GoogleReCaptchaProvider>
+						)}
+					</>
 				)}
+
+				{/* Vaccination Records */}
+				{/*
+					 Todo: 
+					 - create vaccination card component with vaccine name, valid to date, maybe an image for the top of the card
+					 - be able to filter by pet
+				*/}
+
+				{/* Bookings */}
+				{/*
+					 Todo: 
+					 - create vaccination card component with vaccine name, valid to date, maybe an image for the top of the card
+					 - be able to filter by pet
+				*/}
 			</div>
 		</div >
 	)
