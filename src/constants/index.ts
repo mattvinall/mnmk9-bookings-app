70a54c99f2e4fe1e8305b0950bd8da4f699e43c9
mattv993 <matt.vinall7@gmail.com>
2023-07-07 23:02:36 -0400

    added vaccination name options

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,6 +4,16 @@ export const temperamentOptions = [
     "PASSIVE",
     "NEUTRAL",
     "AGGRESSIVE",
-  ];
+];
+
+export const vaccinationOptions = [
+  "Rabies",
+  "Leptospirosis",
+  "Canine parvovirus",
+  "Infliuenza",
+  "Bordaetella",
+  "Hepatitis",
+  "All (Image provided)"
+]
 
 export const rows = 2;
