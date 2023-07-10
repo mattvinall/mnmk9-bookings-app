99a79aaf2d0ecfaa2a98b1803d11f20d1a56a5c6
mattv993 <matt.vinall7@gmail.com>
2023-07-10 14:17:15 -0400

    if user role is admin, render the AdminBookingForm component

--- src/pages/create-booking/index.tsx
+++ src/pages/create-booking/index.tsx
@@ -1,12 +1,15 @@
+"use client";
+
 import { type NextPage } from "next";
 import Link from "next/link";
-import { useSession } from "next-auth/react";
 import Image from "next/image";
 import training from "../../../public/training.png"
 import boarding from "../../../public/boarding.jpg";
 import grooming from "../../../public/grooming.jpg";
 import daycare from "../../../public/daycare.webp";
-import { useUser } from "@clerk/nextjs";
+import { useAuth } from "@clerk/nextjs";
+import { getUserById } from "../../api/users";
+import AdminBookingForm from "../../components/admin/forms/AdminBookingForm";
 
 const cards = [
   {
@@ -14,14 +17,14 @@ const cards = [
     href: "/create-booking/boarding",
     text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
     imageSrc: boarding,
-    price: "$30/night",
+    price: "$40/night",
   },
   {
     service: "Daycare",
     href: "/create-booking/daycare",
     text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
     imageSrc: daycare,
-    price: "$20/day",
+    price: "$25/day",
   },
   {
     service: "Grooming",
@@ -35,54 +38,62 @@ const cards = [
     href: "/create-booking/training",
     text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
     imageSrc: training,
-    price: "$60/hr",
+    price: "$100/hr",
   }
 ]
 
 const CreateBooking: NextPage = () => {
-  const { isSignedIn } = useUser();
+  const { isSignedIn, userId } = useAuth();
+  const { data: userData } = getUserById(userId as string);
   return (
     <>
-      {
-        isSignedIn ? (
-          <div className="container flex flex-col items-center justify-start gap-12 px-4 py-16">
-            <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-              Book a  <span className="text-[rgb(103,163,161)]">Service</span>
-            </h1>
-            <h2 className="text-3xl font-bold text-white text-center">Select a Service that you want to book</h2>
-            <div className="grid grid-cols-1 gap-4 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 md:gap-8 my-20">
-              {
-                cards && cards.map(card => {
-                  return (
-                    <Link
-                      className="flex max-w-sm flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-                      href={card.href}
-                      key={card.service}
-                    >
-                      <div className="flex justify-center">
-                        <div className="rounded-lg shadow-lg bg-white max-w-md">
-                          <Image className="rounded-t-lg" style={{ height: "300px", objectFit: "cover" }} src={card.imageSrc} alt="" />
-                          <div className="p-6">
-                            <h2 className="text-gray-900 text-xl font-medium mb-2">{card.service}</h2>
-                            <p className="text-gray-700 text-base mb-4">
-                              {card.text}
-                            </p>
-                            <p className="text-gray-600 font-bold text-xs">{card.price}</p>
+      {userData?.role === "user" ? (
+        <>
+          {
+            isSignedIn ? (
+              <div className="container flex flex-col items-center justify-start gap-12 px-4 py-16">
+                <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+                  Book a  <span className="text-[rgb(103,163,161)]">Service</span>
+                </h1>
+                <h2 className="text-3xl font-bold text-white text-center">Select a Service that you want to book</h2>
+                <div className="grid grid-cols-1 gap-4 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 md:gap-8 my-20">
+                  {
+                    cards && cards.map(card => {
+                      return (
+                        <Link
+                          className="flex max-w-sm flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+                          href={card.href}
+                          key={card.service}
+                        >
+                          <div className="flex justify-center">
+                            <div className="rounded-lg shadow-lg bg-white max-w-md">
+                              <Image className="rounded-t-lg" style={{ height: "300px", objectFit: "cover" }} src={card.imageSrc} alt="" />
+                              <div className="p-6">
+                                <h2 className="text-gray-900 text-xl font-medium mb-2">{card.service}</h2>
+                                <p className="text-gray-700 text-base mb-4">
+                                  {card.text}
+                                </p>
+                                <p className="text-gray-600 font-bold text-xs">{card.price}</p>
+                              </div>
+                            </div>
                           </div>
-                        </div>
-                      </div>
-                    </Link>
-                  )
-                })
-              }
-            </div>
-          </div>
-        ) : (
-          <div className="container flex flex-col items-center text-center justify-start gap-12 px-4 py-[32vh]">
-            <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">Please Login to book a service</h1>
-          </div>
-        )
-      }
+                        </Link>
+                      )
+                    })
+                  }
+                </div>
+              </div>
+            ) : (
+              <div className="container flex flex-col items-center text-center justify-start gap-12 px-4 py-[32vh]">
+                <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">Please Login to book a service</h1>
+              </div>
+            )
+          }
+        </>
+      ) : (
+        <AdminBookingForm />
+      )}
+
     </>
   );
 };
