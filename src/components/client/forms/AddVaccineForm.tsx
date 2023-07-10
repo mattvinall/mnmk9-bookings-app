d692e4acf3ae9c49c19abb6b8f0ad32bd092fb2d
mattv993 <matt.vinall7@gmail.com>
2023-07-10 11:37:36 -0400

    moved button outside container; caused it to be 100% width

--- src/components/client/forms/AddVaccineForm.tsx
+++ src/components/client/forms/AddVaccineForm.tsx
@@ -163,13 +163,13 @@ const AddVaccineForm = ({ petId, petName, secret, refetch }: Props) => {
                         required
                     />
                 </div>
-                <button
-                    disabled={isSubmitting}
-                    type="submit"
-                    className="mt-[25px] rounded-full bg-gradient-to-l from-[#67A3A1] to-[#112B4E] hover:bg-gradient-to-r from-[#112B4E] to-[#67A3A1] px-16 py-3 font-semibold text-white no-underline transition py-3 px-5 text-sm font-medium text-center rounded-lg bg--700 sm:w-fit focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                    Submit
-                </button>
             </div>
+            <button
+                disabled={isSubmitting}
+                type="submit"
+                className="mt-[25px] rounded-full bg-gradient-to-l from-[#67A3A1] to-[#112B4E] hover:bg-gradient-to-r from-[#112B4E] to-[#67A3A1] px-16 py-3 font-semibold text-white no-underline transition py-3 px-5 text-sm font-medium text-center rounded-lg bg--700 sm:w-fit focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                Submit
+            </button>
         </form>
     )
 }
