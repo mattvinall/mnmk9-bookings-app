cd9d183c627293f567e626888a7106788a14e578
mattv993 <matt.vinall7@gmail.com>
2023-07-10 21:49:29 -0400

    get service by id procedure

--- src/server/trpc/router/service.ts
+++ src/server/trpc/router/service.ts
@@ -1,7 +1,22 @@
+import { z } from "zod";
 import { router, protectedProcedure } from "../trpc";
 
 export const serviceRouter = router({
 	getAllServices: protectedProcedure.query(({ ctx }) => {
 		return ctx.prisma.services.findMany();
-	})
+	}),
+	byId: protectedProcedure
+		.input(
+			z.object({
+				id: z.string(),
+			})
+		)
+		.query(({ ctx, input }) => { 
+			return ctx.prisma.services.findUnique({
+				where: {
+					id: input.id,
+				},
+			});
+		})
+	
 });
