34f112934c682899e80f6079a6a5a4828bb01a83
mattv993 <matt.vinall7@gmail.com>
2023-07-12 11:13:03 -0400

    changed verbiage to select a client

--- src/components/admin/forms/AdminBookingForm.tsx
+++ src/components/admin/forms/AdminBookingForm.tsx
@@ -65,7 +65,7 @@ const AdminBookingForm = ({ secret }: Props) => {
                                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-gray-100 focus:outline-none focus:ring-0 focus:border-gray-100 peer"
                                     onChange={handleChange}
                                 >
-                                    <option value="">Select a user</option>
+                                    <option value="">Select a client</option>
                                     {users?.map((user: User) => (
                                         <option key={user.id} value={user.id}>
                                             {user.name}
