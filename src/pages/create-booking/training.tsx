bfe10492ddcc97f2d5ea27346b1e6c180832d56c
mattv993 <matt.vinall7@gmail.com>
2023-07-17 21:20:53 -0400

    training logic to add to invoice table on success of booking

--- src/pages/create-booking/training.tsx
+++ src/pages/create-booking/training.tsx
@@ -38,7 +38,7 @@ const Training: NextPage = () => {
 	const training = serviceData?.find(service => service.serviceName === "Training");
 
 	const trainingId = training?.id as string;
-	const trainingPrice = training?.price as number;
+	const trainingPrice = Number(training?.price);
 
 	// query the pets table and find the 
 	const { data: petData, isLoading, error } = trpc.pet.byOwnerId.useQuery({ id: userId as string }, {
@@ -57,31 +57,40 @@ const Training: NextPage = () => {
 		},
 	});
 
+	const { mutate: createInvoice } = trpc.invoice.create.useMutation();
+
 	const addNewTrainingBooking = trpc.bookings.newBooking.useMutation({
 		onSuccess: (data) => {
 			const checkInDate = new Date(data?.checkInDate as string);
 			const checkOutDate = new Date(data?.checkOutDate as string);
 			const serviceDuration = calculateServiceDuration(checkInDate, checkOutDate);
+			const subtotal = calculateSubtotal(trainingPrice, serviceDuration);
+			const taxAmount = calculateTaxAmount(subtotal);
+			const total = calculateTotalAmount(subtotal + taxAmount);
 
-			const invoice = {
-				bookingId: data?.id as string,
-				petName: data?.petName as string,
-				serviceName: data?.serviceName as string,
-				servicePrice: trainingPrice,
-				serviceDuration: serviceDuration,
-				customerName: `${data?.firstName} ${data?.lastName}`,
-				customerEmail: data?.email as string,
-				customerAddress: userData?.address as string,
-				customerCity: userData?.city as string,
-				subtotal: calculateSubtotal(trainingPrice, serviceDuration) as number,
-				taxAmount: calculateTaxAmount(calculateSubtotal(trainingPrice, serviceDuration) as number),
-				total: calculateTotalAmount(calculateSubtotal(trainingPrice, serviceDuration) as number),
-				createdAt: new Date().toLocaleDateString() as string,
-				dueDate: data?.checkOutDate && new Date(data?.checkOutDate).toLocaleDateString() as string,
-			}
+			try {
 
-			if (invoice) {
-				generateInvoice(invoice as Invoice);
+				createInvoice({
+					bookingId: data?.id as string,
+					petId: data?.petId as string,
+					petName: data?.petName as string,
+					clientId: data?.userId as string,
+					serviceId: trainingId as string,
+					serviceName: data?.serviceName as string,
+					servicePrice: trainingPrice,
+					serviceDuration: serviceDuration,
+					customerName: `${data?.firstName} ${data?.lastName}`,
+					customerEmail: data?.email as string,
+					customerAddress: userData?.address as string,
+					customerCity: userData?.city as string,
+					subtotal: subtotal,
+					taxAmount: taxAmount,
+					total: total,
+					createdAt: new Date().toLocaleDateString() as string,
+					dueDate: data?.checkOutDate ? new Date(data?.checkOutDate).toLocaleDateString() as string : "At Checkout",
+				});
+			} catch (error) {
+				console.log("error creating invoice", error);
 			}
 		}
 	});
