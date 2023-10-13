404fd94a0eebe89e51d0de39eca39a989e52b3b6
Matt Vinall <matt.vinall7@gmail.com>
2023-10-13 14:24:08 -0400

    sstyle updates

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -134,10 +134,10 @@ const Users = () => {
                     <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
                         MNMK-9  <span className="text-[rgb(238,182,43)]">Users</span>
                     </h1>
-                    <div className="flex justify-between">
+                    <div className="flex justify-between items-center w-3/4">
                         <>
                             <label htmlFor="simple-search" className="sr-only">Search</label>
-                            <div className="relative w-[35%]">
+                            <div className="relative w-[35%] mr-4">
                                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                                     <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-100" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                                 </div>
@@ -148,7 +148,7 @@ const Users = () => {
                             +
                         </button>
                     </div>
-                    {/* {showAddUserForm ? (
+                    {showAddUserForm ? (
                         <form onSubmit={handleSubmit(onSubmit)}>
                             <div className="grid md:grid-cols-2 md:gap-6">
                                 <div className="relative z-0 mb-6 w-full group">
@@ -187,7 +187,7 @@ const Users = () => {
                                 Add User
                             </button>
                         </form>
-                    ) : null} */}
+                    ) : null}
 
                     {/* display results */}
                     <ul className="grid grid-cols-1 gap-4 lg:grid-cols-3 md:grid-cols-2 md:gap-8 mt-10">
