aa5e41cf6e41e40d6202b8d172a5d133a5264474
Matt Vinall <matt.vinall7@gmail.com>
2023-10-13 14:30:10 -0400

    test commit

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -74,21 +74,23 @@ const Users = () => {
     };
 
     type AddNewUserFormType = {
-        newUserName: string;
-        newUserEmail: string;
+        name: string;
+        email: string;
     }
+
     const addNewUserFormSchema = z.object({
-        newUserName: z.string().min(2, "Name must be at least 2 characters long").max(50, "Name must be less than 50 characters long"),
-        newUserEmail: z.string().email("Please enter a valid email address")
-    })
+        name: z.string().min(2, "Name must be at least 2 characters long").max(50, "Name must be less than 50 characters long"),
+        email: z.string().email("Please enter a valid email address")
+    });
+
     const { register, handleSubmit, reset } = useForm<AddNewUserFormType>({
         resolver: zodResolver(addNewUserFormSchema)
     });
 
     const onSubmit: SubmitHandler<AddNewUserFormType> = async (formData: AddNewUserFormType) => {
         try {
-            const { newUserName, newUserEmail } = formData;
-            addUser({ name: newUserName, email: newUserEmail });
+            const { name, email } = formData;
+            addUser({ name, email });
             reset();
 
             // success message 
@@ -137,14 +139,14 @@ const Users = () => {
                     <div className="flex justify-between items-center w-3/4">
                         <>
                             <label htmlFor="simple-search" className="sr-only">Search</label>
-                            <div className="relative w-[35%] mr-4">
+                            <div className="relative w-full mr-4">
                                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                                     <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-100" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                                 </div>
                                 <input onChange={handleSearch} value={searchTerm} type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search by user or pet..." required />
                             </div>
                         </>
-                        <button onClick={handleAddUser} className="bg-gray-900 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4">
+                        <button onClick={handleAddUser} className="bg-gray-900 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
                             +
                         </button>
                     </div>
@@ -156,7 +158,7 @@ const Users = () => {
                                         className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-gray-100 focus:outline-none focus:ring-0 focus:border-gray-100 peer"
                                         type="text"
                                         id="new-user-name"
-                                        {...register("newUserName")}
+                                        {...register("name")}
                                     />
                                     <label
                                         htmlFor="new-user-name"
@@ -171,7 +173,7 @@ const Users = () => {
                                         className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-500 dark:focus:border-gray-100 focus:outline-none focus:ring-0 focus:border-gray-100 peer"
                                         type="email"
                                         id="new-user-email"
-                                        {...register("newUserEmail")}
+                                        {...register("email")}
                                     />
                                     <label
                                         className="peer-focus:font-medium absolute text-sm text-gray-100 dark:text-gray-100 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-gray-100 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
