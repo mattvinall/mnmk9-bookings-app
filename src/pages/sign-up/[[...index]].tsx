aa364b66e5814f37fa25caa09dd7dc6e0b21b3b3
Matt Vinall <matt.vinall7@gmail.com>
2023-11-24 19:59:56 -0500

    test

--- src/pages/sign-up/[[...index]].tsx
+++ src/pages/sign-up/[[...index]].tsx
@@ -1,7 +1,13 @@
 import { SignUp } from "@clerk/nextjs";
 
 const SignUpPage = () => (
-	<SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
+	<SignUp path="/sign-up" routing="path" signInUrl="/sign-in" appearance={{
+		elements: {
+			footer: {
+				display: 'none',
+			},
+		},
+	}} />
 );
 
 export default SignUpPage;
