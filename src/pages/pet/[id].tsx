08135cd68480a8458025c7682e5f58966db04c8d
Matt Vinall <matt.vinall7@gmail.com>
2023-12-07 10:58:30 -0500

    remove console.logs

--- src/pages/pet/[id].tsx
+++ src/pages/pet/[id].tsx
@@ -33,13 +33,9 @@ const PetDetail: NextPage = () => {
 	const ownerId = petDetail?.map((pet: Pet) => pet.ownerId as string)[0];
 	const vaccinationRecords = petDetail && petDetail?.map((pet: PetDetail) => pet?.vaccinations)[0] || [];
 
-	console.log("pet id used to call waiver API", petId);
-
 	const { data: waiverByPetId } = trpc.waiver.byPetId.useQuery({ petId: petId as string });
-	console.log("waiverByPetId", waiverByPetId);
 
 	const waiverFormSignedStatus = waiverByPetId && Object.keys(waiverByPetId)?.length > 0 ? true : false;
-	console.log("waiverFormSigned boolean status", waiverFormSignedStatus);
 
 	useEffect(() => {
 		const key = process.env.NEXT_PUBLIC_RECAPTCHA_SITEKEY;
