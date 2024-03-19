f5e3ecf84fe77988b7e083a0b58a5cbfbe70df00
Matt Vinall <matt.vinall7@gmail.com>
2024-03-19 14:59:00 -0400

    fix error on router

--- src/types/router.ts
+++ src/types/router.ts
@@ -1,7 +1,8 @@
 import { RouterOutputs } from "../utils/trpc";
 
-export type BookingsArray = RouterOutputs["bookings"]["getAllBookings"]
-export type Booking = RouterOutputs["bookings"]["getAllBookings"][number]
+export type BookingsArray = RouterOutputs["bookings"]["getAllBookings"];
+export type Booking =
+  RouterOutputs["bookings"]["getAllBookings"] extends (infer R)[] ? R : never;
 
-export type PetsArray = RouterOutputs["pet"]["getAllPets"]
-export type Pet = RouterOutputs["pet"]["getAllPets"][number]
+export type PetsArray = RouterOutputs["pet"]["getAllPets"];
+export type Pet = RouterOutputs["pet"]["getAllPets"][number];
