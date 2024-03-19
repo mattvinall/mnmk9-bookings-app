bf2e52e85e415fa3b5df19cdd94af34dff7b6648
Matt Vinall <matt.vinall7@gmail.com>
2024-03-19 14:50:02 -0400

    remove cache logic from booking rotuer

--- src/server/trpc/router/booking.ts
+++ src/server/trpc/router/booking.ts
@@ -1,231 +1,254 @@
 import { z } from "zod";
 
 import { router, protectedProcedure } from "../trpc";
-import { TRPCError } from '@trpc/server';
-import { rateLimit } from './../../../lib/rateLimit';
-import { getCache, setCache, invalidateCache } from "../../../lib/cache";
+import { TRPCError } from "@trpc/server";
+import { rateLimit } from "./../../../lib/rateLimit";
+// import { getCache, setCache, invalidateCache } from "../../../lib/cache";
 
 export const bookingRouter = router({
-	getAllBookings: protectedProcedure.query(async ({ ctx }) => {
-		try {
-			const cache = await getCache("allBookings");
+  getAllBookings: protectedProcedure.query(async ({ ctx }) => {
+    try {
+      const allBookings = await ctx.prisma.bookings.findMany();
+      return allBookings;
+    } catch (error) {
+      console.log(`failed to fetch all bookings: ${error}`);
+    }
+  }),
+  byId: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      try {
+        const { id } = input;
+        return await ctx.prisma.bookings.findUnique({ where: { id } });
+      } catch (error) {
+        console.log(`Booking cannot be fetched by ID: ${error}`);
+      }
+    }),
+  newBooking: protectedProcedure
+    .input(
+      z.object({
+        firstName: z.string(),
+        lastName: z.string(),
+        phoneNumber: z.string(),
+        email: z.string(),
+        checkInDate: z.string(),
+        checkOutDate: z.string().optional(),
+        startTime: z.string().optional(),
+        endTime: z.string().optional(),
+        petName: z.string(),
+        notes: z.string().optional(),
+        serviceName: z.string(),
+        petId: z.string(),
+        serviceId: z.string(),
+        userId: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const {
+          firstName,
+          lastName,
+          phoneNumber,
+          email,
+          checkInDate,
+          checkOutDate,
+          petName,
+          notes,
+          startTime,
+          endTime,
+          serviceName,
+          petId,
+          serviceId,
+          userId,
+        } = input;
+        const { success } = await rateLimit.limit(userId);
 
-			if (cache) {
-				return cache;
-			} else {
-				const allBookings = await ctx.prisma.bookings.findMany();
+        if (!success) {
+          throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+        }
 
-				await setCache("allBookings", allBookings);
-				return allBookings;
-			}
-		} catch (error) {
-			console.log(`failed to fetch all bookings: ${error}`)
-		}
-	}),
-	byId: protectedProcedure
-		.input(z.object({ id: z.string() }))
-		.query(async ({ ctx, input }) => {
-			try {
-				const { id } = input;
-				return await ctx.prisma.bookings.findUnique({ where: { id } })
-			} catch (error) {
-				console.log(`Booking cannot be fetched by ID: ${error}`)
-			}
-		}),
-	newBooking: protectedProcedure
-		.input(
-			z.object({
-				firstName: z.string(),
-				lastName: z.string(),
-				phoneNumber: z.string(),
-				email: z.string(),
-				checkInDate: z.string(),
-				checkOutDate: z.string().optional(),
-				startTime: z.string().optional(),
-				endTime: z.string().optional(),
-				petName: z.string(),
-				notes: z.string().optional(),
-				serviceName: z.string(),
-				petId: z.string(),
-				serviceId: z.string(),
-				userId: z.string(),
-			})
-		)
-		.mutation(async ({ ctx, input }) => {
-			try {
+        // // invalidate cache
+        // await invalidateCache("allBookings");
+        // await invalidateCache(`user-${userId}}`);
 
-				const { firstName, lastName, phoneNumber, email, checkInDate, checkOutDate, petName, notes, startTime, endTime, serviceName, petId, serviceId, userId } = input
-				const { success } = await rateLimit.limit(userId)
+        return await ctx.prisma.bookings.create({
+          data: {
+            firstName,
+            lastName,
+            phoneNumber,
+            email,
+            checkInDate,
+            checkOutDate,
+            startTime,
+            endTime,
+            petName,
+            notes,
+            serviceName,
+            petId,
+            serviceId,
+            userId,
+          },
+        });
+      } catch (error) {
+        console.log(`booking could not be created: ${error}`);
+      }
+    }),
+  newAdminBooking: protectedProcedure
+    .input(
+      z.object({
+        firstName: z.string(),
+        lastName: z.string(),
+        phoneNumber: z.string(),
+        email: z.string(),
+        checkInDate: z.string(),
+        checkOutDate: z.string(),
+        startTime: z.string(),
+        endTime: z.string(),
+        petName: z.string(),
+        notes: z.string().optional(),
+        serviceName: z.string(),
+        petId: z.string(),
+        serviceId: z.string(),
+        userId: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const {
+          firstName,
+          lastName,
+          phoneNumber,
+          email,
+          checkInDate,
+          checkOutDate,
+          petName,
+          notes,
+          startTime,
+          endTime,
+          serviceName,
+          petId,
+          serviceId,
+          userId,
+        } = input;
+        const { success } = await rateLimit.limit(userId);
 
-				if (!success) {
-					throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
-				}
+        if (!success) {
+          throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+        }
 
-				// invalidate cache
-				await invalidateCache("allBookings");
-				await invalidateCache(`user-${userId}}`);
+        // await invalidateCache("allBookings");
+        // await invalidateCache(`user-${userId}}`);
 
-				return await ctx.prisma.bookings.create({
-					data: {
-						firstName,
-						lastName,
-						phoneNumber,
-						email,
-						checkInDate,
-						checkOutDate,
-						startTime,
-						endTime,
-						petName,
-						notes,
-						serviceName,
-						petId,
-						serviceId,
-						userId
-					}
-				})
-			} catch (error) {
-				console.log(`booking could not be created: ${error}`)
-			}
-		}),
-	newAdminBooking: protectedProcedure
-		.input(
-			z.object({
-				firstName: z.string(),
-				lastName: z.string(),
-				phoneNumber: z.string(),
-				email: z.string(),
-				checkInDate: z.string(),
-				checkOutDate: z.string(),
-				startTime: z.string(),
-				endTime: z.string(),
-				petName: z.string(),
-				notes: z.string().optional(),
-				serviceName: z.string(),
-				petId: z.string(),
-				serviceId: z.string(),
-				userId: z.string(),
-			}))
-		.mutation(async ({ ctx, input }) => { 
-			try {
-				const { firstName, lastName, phoneNumber, email, checkInDate, checkOutDate, petName, notes, startTime, endTime, serviceName, petId, serviceId, userId } = input
-				const { success } = await rateLimit.limit(userId)
+        return await ctx.prisma.bookings.create({
+          data: {
+            firstName,
+            lastName,
+            phoneNumber,
+            email,
+            checkInDate,
+            checkOutDate,
+            startTime,
+            endTime,
+            petName,
+            notes,
+            serviceName,
+            petId,
+            serviceId,
+            userId,
+          },
+        });
+      } catch (error) {
+        console.log(`booking could not be created: ${error}`);
+      }
+    }),
 
-				if (!success) {
-					throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
-				}
+  editBooking: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        checkInDate: z.string().optional(),
+        checkOutDate: z.string().optional(),
+        startTime: z.string().optional(),
+        endTime: z.string().optional(),
+        notes: z.string().optional(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id, checkInDate, checkOutDate, startTime, endTime, notes } =
+        input;
 
-				await invalidateCache("allBookings");
-				await invalidateCache(`user-${userId}}`);
+      try {
+        const { success } = await rateLimit.limit(id);
 
-				return await ctx.prisma.bookings.create({
-					data: {
-						firstName,
-						lastName,
-						phoneNumber,
-						email,
-						checkInDate,
-						checkOutDate,
-						startTime,
-						endTime,
-						petName,
-						notes,
-						serviceName,
-						petId,
-						serviceId,
-						userId
-					}
-				})
-			} catch (error) {
-				console.log(`booking could not be created: ${error}`)
-			}
-		}),
+        if (!success) {
+          throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+        }
 
-	editBooking: protectedProcedure
-		.input(
-			z.object({
-				id: z.string(),
-				checkInDate: z.string().optional(),
-				checkOutDate: z.string().optional(),
-				startTime: z.string().optional(),
-				endTime: z.string().optional(),
-				notes: z.string().optional()
-			})
-		)
-		.mutation(async ({ ctx, input }) => {
-			const { id, checkInDate, checkOutDate, startTime, endTime, notes } = input;
+        // invalidate cache
+        // await invalidateCache("allBookings");
 
-			try {
-				const { success } = await rateLimit.limit(id)
+        return await ctx.prisma.bookings.update({
+          where: {
+            id,
+          },
+          data: {
+            checkInDate,
+            checkOutDate,
+            startTime,
+            endTime,
+            notes,
+          },
+        });
+      } catch (error) {
+        console.log(`booking could not be edited: ${error}`);
+      }
+    }),
+  cancelBooking: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id } = input;
 
-				if (!success) {
-					throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
-				}
+      try {
+        // invalidate cache
+        // await invalidateCache("allBookings");
 
-				// invalidate cache
-				await invalidateCache("allBookings");
+        return await ctx.prisma.bookings.delete({
+          where: {
+            id,
+          },
+        });
+      } catch (error) {
+        console.log(`booking could not be cancelled/deleted: ${error}`);
+      }
+    }),
+  confirmBooking: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        confirmedBooking: z.boolean(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { id, confirmedBooking } = input;
 
-				return await ctx.prisma.bookings.update({
-					where: {
-						id,
-					},
-					data: {
-						checkInDate,
-						checkOutDate,
-						startTime,
-						endTime,
-						notes
-					}
-				})
-			} catch (error) {
-				console.log(`booking could not be edited: ${error}`)
-			}
-		}),
-	cancelBooking: protectedProcedure
-		.input(
-			z.object({
-				id: z.string()
-			})
-		)
-		.mutation(async ({ ctx, input }) => {
-			const { id } = input;
+      try {
+        // invalidate cache
+        // await invalidateCache("allBookings");
 
-			try {
-				// invalidate cache
-				await invalidateCache("allBookings");
-
-				return await ctx.prisma.bookings.delete({
-					where: {
-						id
-					}
-				})
-			} catch (error) {
-				console.log(`booking could not be cancelled/deleted: ${error}`)
-			}
-		}),
-	confirmBooking: protectedProcedure
-		.input(
-			z.object({
-				id: z.string(),
-				confirmedBooking: z.boolean()
-			})
-		)
-		.mutation(async ({ ctx, input }) => {
-			const { id, confirmedBooking } = input;
-
-			try {
-				// invalidate cache
-				await invalidateCache("allBookings");
-
-				return await ctx.prisma.bookings.update({
-					where: {
-						id
-					},
-					data: {
-						confirmedBooking: !confirmedBooking
-					}
-				});
-			} catch (error) {
-				console.log(`error updating booking status to confirmed: ${error}`)
-			}
-		})
+        return await ctx.prisma.bookings.update({
+          where: {
+            id,
+          },
+          data: {
+            confirmedBooking: !confirmedBooking,
+          },
+        });
+      } catch (error) {
+        console.log(`error updating booking status to confirmed: ${error}`);
+      }
+    }),
 });
